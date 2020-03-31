6d9f9ab48bfd82f2deb1ba95371d9d65e4f7f92e
Matthew Law <matthew.law@distinction.co.uk>
2020-03-31 09:59:51 +0100

    Backend integration for postcode checker

---
+++ HelpMyStreetFE/HelpMyStreetFE/Controllers/PostCodeController.cs
@@ -0,0 +1,29 @@
+﻿using HelpMyStreetFE.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace HelpMyStreetFE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostCodeController : ControllerBase
+    {
+        private readonly ILogger<PostCodeController> _logger;
+        private readonly IAddressService _addressService;
+
+        public PostCodeController(ILogger<PostCodeController> logger, IAddressService addressService)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _addressService = addressService ?? throw new ArgumentNullException(nameof(addressService));
+        }
+
+        [HttpGet("{postCode}")]
+        public async Task<ActionResult<PostCodeResponse>> Get(string postCode)
+        {
+            _logger.LogInformation($"PostCode {postCode}");
+            return await _addressService.CheckPostCode(postCode);
+        }
+    }
+}
--- HelpMyStreetFE/HelpMyStreetFE/src/js/app.js
+++ HelpMyStreetFE/HelpMyStreetFE/src/js/app.js
@@ -1,3 +1,35 @@
 ﻿import '../sass/main.scss';
 
-console.log('Hello world');
+$(function () {
+    $("#postcode_button").click(function (evt) {
+
+        const postCode = $("#postcode").val();
+
+        if (postCode) {
+            $(this).width($(this).width());
+            $(this).height($(this).height());
+
+            $('#postcode_notcovered').hide();
+            $('#postcode_covered').hide();
+            $('#postcode_error').hide();
+            $(this).find('.text').hide();
+            $(this).find('.loader').show();
+
+            fetch(`/api/postcode/${postCode}`)
+                .then(resp => resp.json())
+                .then(data => {
+                    if (data.addresses && data.addresses.length) $('#postcode_covered').show();
+                    else $('#postcode_notcovered').show();
+                })
+                .catch(err => {
+                    $('#postcode_error').show();
+                })
+                .finally(() => {
+                    $(this).width(null);
+                    $(this).height(null);
+                    $(this).find('.text').show();
+                    $(this).find('.loader').hide();
+                });
+        }
+    });
+});
