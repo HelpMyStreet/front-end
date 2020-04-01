1ebaa2264c7d21862855e1dfeea263b08d13cc59
Kieran Gookey <kgookey@gmail.com>
2020-04-01 14:02:09 +0100

    Added account header, account notifications, babel to transpile es2016+ into es2015, a new logo that teo got me +semver:major

---
+++ HelpMyStreetFE/HelpMyStreetFE/Models/Account/NotificationModel.cs
@@ -0,0 +1,16 @@
+﻿using HelpMyStreetFE.Enums.Account;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelpMyStreetFE.Models.Account
+{
+    public class NotificationModel
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Message { get; set; }
+        public NotificationType Type { get; set; }
+    }
+}
