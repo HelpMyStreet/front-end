fbb7ca09e71ef61ae95ac4494ad29a9b13f4a2d9
kev_durber <kevin.durber@podium-solutions.co.uk>
2020-04-01 14:08:10 +0100

    Profile component tweaks

--- HelpMyStreetFE/HelpMyStreetFE/Controllers/AccountController.cs
+++ HelpMyStreetFE/HelpMyStreetFE/Controllers/AccountController.cs
@@ -1,10 +1,4 @@
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Threading.Tasks;
-using HelpMyStreetFE.Models;
-using HelpMyStreetFE.Services;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace HelpMyStreetFE.Controllers {
   public class AccountController : Controller {
