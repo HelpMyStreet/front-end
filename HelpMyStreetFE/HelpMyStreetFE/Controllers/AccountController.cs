9d074ce7c378b77e3195ffc6a927bf7c23948881
Jawwad Mukhtar <jawwad.mukhtar@gmail.com>
2020-04-06 13:01:21 +0100

    Changes to account controller

--- HelpMyStreetFE/HelpMyStreetFE/Controllers/AccountController.cs
+++ HelpMyStreetFE/HelpMyStreetFE/Controllers/AccountController.cs
@@ -7,6 +7,8 @@ using HelpMyStreetFE.Enums.Account;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+using HelpMyStreetFE.Services;
 
 namespace HelpMyStreetFE.Controllers
 {
@@ -14,20 +16,70 @@ namespace HelpMyStreetFE.Controllers
     public class AccountController : Controller
     {
         private readonly ILogger<AccountController> _logger;
-        public AccountController(ILogger<AccountController> logger)
+        private readonly IUserService _userService;
+        public AccountController(
+            ILogger<AccountController> logger,
+            IUserService userService
+            )
         {
             _logger = logger;
+            _userService = userService;
+        }
+
+        private UserDetails GetUserDetails(HelpMyStreet.Utils.Models.User user)
+        {
+            var personalDetails = user.UserPersonalDetails;
+            string initials = personalDetails.FirstName.Substring(0, 1) + personalDetails.LastName.Substring(0, 1);
+            string address = personalDetails.Address.AddressLine1 + "," + personalDetails.Address.Postcode;
+            string gender = "XXX";
+            bool underlyingMedicalConditions = false;
+
+            if(personalDetails.UnderlyingMedicalCondition.HasValue)
+            {
+                underlyingMedicalConditions = personalDetails.UnderlyingMedicalCondition.Value;
+            }
+                    
+            return new UserDetails(
+                initials,
+                personalDetails.DisplayName,
+                personalDetails.FirstName,
+                personalDetails.LastName,
+                personalDetails.EmailAddress,
+                address,
+                personalDetails.MobilePhone,
+                personalDetails.OtherPhone,
+                personalDetails.DateOfBirth.Value.ToString(),
+                gender,
+                underlyingMedicalConditions
+                );
         }
 
         [HttpGet]
         public IActionResult Index()
         {
             //TODO: Check that the user is authenticated
+            var id = int.Parse(HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value);
+            HelpMyStreet.Utils.Models.User user = _userService.GetUserAsync(id).Result;
 
-            //TODO: Actually get user details
-            var mockDetails = new UserDetails("KG");
-            var mockNotifications = new List<NotificationModel>() { new NotificationModel { Id = Guid.NewGuid(), Title = "Good news John!", Message = "Your account is all set up. From your profile you can claim local streets, search for local volunteers and update your details. Keep checking back and keep an eye on your email inbox for the latest updates to our service. We hope to be distributing requests for help very soon.", Type = NotificationType.Success } };
-            var viewModel = new AccountViewModel() { UserDetails = mockDetails, Notifications = mockNotifications };
+            AccountViewModel viewModel = new AccountViewModel();
+
+            if (user!=null)
+            {
+                var userDetails = GetUserDetails(user);
+
+                List<NotificationModel> notifications = new List<NotificationModel>() 
+                { 
+                    new NotificationModel 
+                    {
+                        Id = Guid.NewGuid(), 
+                        Title = "Good news " + user.UserPersonalDetails.FirstName +"!", 
+                        Message = "Your account is all set up. From your profile you can claim local streets, search for local volunteers and update your details. Keep checking back and keep an eye on your email inbox for the latest updates to our service. We hope to be distributing requests for help very soon.", 
+                        Type = NotificationType.Success 
+                    }
+                };
+                viewModel.UserDetails = userDetails;
+                viewModel.Notifications = notifications;
+            }
             return View(viewModel);
         }
 
--- HelpMyStreetFE/HelpMyStreetFE/Models/Account/UserDetails.cs
+++ HelpMyStreetFE/HelpMyStreetFE/Models/Account/UserDetails.cs
@@ -2,11 +2,44 @@
 {
     public class UserDetails //I expect this class to be depricated when we know how to differentiate users
     {
-        public UserDetails(string initials)
+        public UserDetails(
+            string initials, 
+            string displayName, 
+            string firstName, 
+            string lastName,
+            string emailAddress,
+            string address,
+            string mobileNumber,
+            string otherNumber,
+            string dateOfBirth,
+            string gender,
+            bool underlyingMedicalConditions
+            )
         {
             Initials = initials;
+            DisplayName = displayName;
+            FirstName = firstName;
+            LastName = lastName;
+            EmailAddress = emailAddress;
+            MobileNumber = mobileNumber;
+            OtherNumber = otherNumber;
+            DateOfBirth = dateOfBirth;
+            Gender = gender;
+            Address = address;
+            UnderlyingMedicalConditions = underlyingMedicalConditions;
         }
 
         public string Initials { get; set; }
+        public string DisplayName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string Address { get; set; }
+        public string MobileNumber { get; set; }
+        public string OtherNumber { get; set; }
+        public string DateOfBirth { get; set; }
+        public string Gender { get; set; }
+        public bool UnderlyingMedicalConditions { get; set; }
+
     }
 }
