3a46049847e943dc8ceb4ee2b57ecdeb10b4b2a6
kev_durber <kevin.durber@podium-solutions.co.uk>
2020-04-06 18:19:35 +0100

    FE-55 Add content pages. Use common font sizes.

---
+++ HelpMyStreetFE/HelpMyStreetFE/Controllers/PagesController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelpMyStreetFE.Controllers
+{
+  public class PagesController : Controller
+  {
+    public IActionResult Community()
+    {
+      return View();
+    }
+  }
+}
