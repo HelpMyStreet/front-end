00f3f2e6d818f73ff5e569bc45bf8e9b175fa391
Jawwad Mukhtar <jawwad.mukhtar@gmail.com>
2020-04-10 11:28:55 +0100

    formatting

--- HelpMyStreetFE/HelpMyStreetFE/Controllers/AccountController.cs
+++ HelpMyStreetFE/HelpMyStreetFE/Controllers/AccountController.cs
@@ -1,4 +1,4 @@
-﻿﻿using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -36,20 +36,20 @@ namespace HelpMyStreetFE.Controllers
             string gender = "Unknown";
             string underlyingMedicalConditions = "No";
 
-            if(user.ChampionPostcodes.Count>0)
+            if (user.ChampionPostcodes.Count > 0)
             {
                 streetChampion = "Street Champion";
             }
-            else if(user.IsVerified.HasValue && user.IsVerified.Value==true)
+            else if (user.IsVerified.HasValue && user.IsVerified.Value == true)
             {
                 streetChampion = "Volunteer";
             }
 
-            if(personalDetails.UnderlyingMedicalCondition.HasValue)
+            if (personalDetails.UnderlyingMedicalCondition.HasValue)
             {
                 underlyingMedicalConditions = personalDetails.UnderlyingMedicalCondition.Value ? "Yes" : "No";
             }
-                    
+
             return new UserDetails(
                 initials,
                 personalDetails.DisplayName,
@@ -75,18 +75,18 @@ namespace HelpMyStreetFE.Controllers
 
             AccountViewModel viewModel = new AccountViewModel();
 
-            if (user!=null)
+            if (user != null)
             {
                 var userDetails = GetUserDetails(user);
 
-                List<NotificationModel> notifications = new List<NotificationModel>() 
-                { 
-                    new NotificationModel 
+                List<NotificationModel> notifications = new List<NotificationModel>()
+                {
+                    new NotificationModel
                     {
-                        Id = Guid.NewGuid(), 
-                        Title = "Good news " + user.UserPersonalDetails.FirstName +"!", 
-                        Message = "Your account is all set up. From your profile you can claim local streets, search for local volunteers and update your details. Keep checking back and keep an eye on your email inbox for the latest updates to our service. We hope to be distributing requests for help very soon.", 
-                        Type = NotificationType.Success 
+                        Id = Guid.NewGuid(),
+                        Title = "Good news " + user.UserPersonalDetails.FirstName +"!",
+                        Message = "Your account is all set up. From your profile you can claim local streets, search for local volunteers and update your details. Keep checking back and keep an eye on your email inbox for the latest updates to our service. We hope to be distributing requests for help very soon.",
+                        Type = NotificationType.Success
                     }
                 };
                 viewModel.UserDetails = userDetails;
