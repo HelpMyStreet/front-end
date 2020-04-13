6440e30cea663746da1d06233b05526ea6170e2b
James Bloor <james.bloor@distinction.co.uk>
2020-04-13 20:59:21 +0100

    Adding routing for 404 and any other error

--- HelpMyStreetFE/HelpMyStreetFE/Startup.cs
+++ HelpMyStreetFE/HelpMyStreetFE/Startup.cs
@@ -53,7 +53,8 @@ namespace HelpMyStreetFE
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseStatusCodePagesWithReExecute("/Error/{0}");
+
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
