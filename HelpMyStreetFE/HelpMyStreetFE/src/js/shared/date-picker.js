1666de2588b0632aa97e8f546cb7bf007659c095
Usmaan Razaq <usrazaq@gmail.com>
2020-04-15 15:36:15 +0100

    fixed error message on page and bug that i introduced

--- HelpMyStreetFE/HelpMyStreetFE/src/js/shared/date-picker.js
+++ HelpMyStreetFE/HelpMyStreetFE/src/js/shared/date-picker.js
@@ -26,10 +26,13 @@ export function datepickerLoad(id) {
 }
 
 export function validateDob(val, id) {
-    val = val.replace(/\s/g, '');
+
     var regexFormatString = RegExp(/^(([0-9])|([0-2][0-9])|([3][0-1]))\ (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\ \d{4}$/);
     var regex = RegExp(/^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/);
     let stringDate = regexFormatString.test(val);
+    if (!stringDate) {
+        val = val.replace(/\s/g, '');
+    }
     if (regex.test(val) == false && stringDate == false ) {
         $('#' + id).find("~ .error").show();
         $('#' + id).find("~ .error").text("Please enter a valid date of birth in the following format DD/MM/YYYY");
