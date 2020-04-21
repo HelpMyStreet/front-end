f68595be26359f9625753977907966ce95e45a30
Mike Matthews <mike@factor-50.co.uk>
2020-04-21 16:12:55 +0100

    Passing token on success for debugging

--- HelpMyStreetFE/HelpMyStreetFE/src/js/yoti.js
+++ HelpMyStreetFE/HelpMyStreetFE/src/js/yoti.js
@@ -21,11 +21,11 @@ $(() => {
         $('.yoti__auth__button').hide();
         $('.yoti__auth__loading').css("visibility", "visible");
         $('.yoti__auth__loading').css("height", "100%");
-        var response = await fetch("/yoti/ValidateToken" + "?token=" + thisToken + "&u=" + userId);  
+        var response = await fetch("/yoti/ValidateToken" + "?token=" + thisToken + "&u=" + userId);
         if (response.status == 200) {
-            window.location.href = "/yoti/AuthSuccess";
+            window.location.href = "/yoti/AuthSuccess?u=" + userId;
         } else {            
-            window.location.href = "/yoti/AuthFailed?u=" + userId;            
+            window.location.href = "/yoti/AuthFailed?u=" + userId;
         }
     }
 
