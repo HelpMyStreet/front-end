d7ef7d379f46e22697379ae26e973d2bcb6d480e
Usmaan Razaq <usrazaq@gmail.com>
2020-04-24 13:53:25 +0100

    Ported changes across from the arvindan branch

---
+++ HelpMyStreetFE/HelpMyStreetFE/Models/Reponses/GetHelperResponse.cs
@@ -0,0 +1,10 @@
+﻿using HelpMyStreet.Utils.Models;
+using System.Collections.Generic;
+
+namespace HelpMyStreetFE.Models.Reponses
+{
+    public class GetHelperResponse : BaseResponse
+    {
+        public List<User> Users { get; set; }
+    }
+}
--- HelpMyStreetFE/HelpMyStreetFE/Services/UserService.cs
+++ HelpMyStreetFE/HelpMyStreetFE/Services/UserService.cs
@@ -1,5 +1,6 @@
 ﻿using HelpMyStreet.Utils.Enums;
 using HelpMyStreet.Utils.Models;
+using HelpMyStreetFE.Models.Reponses;
 using HelpMyStreetFE.Repositories;
 using Microsoft.Extensions.Logging;
 using System;
@@ -123,6 +124,11 @@ namespace HelpMyStreetFE.Services
         public async Task<int> GetVolunteers()
         {
             return await _userRepository.GetDistinctVolunteerUserCount();
+        }
+
+        public async Task<GetHelperResponse> GetHelpersByPostcode(string postcode)
+        {
+            return await _userRepository.GetHelpersByPostcode(postcode);
         }
     }
 }
