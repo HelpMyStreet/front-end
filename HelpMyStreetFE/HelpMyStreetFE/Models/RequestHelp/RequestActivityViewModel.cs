8f0f8b5aa9d31958d9e3f78876c1b6bc26d880e2
Usmaan Razaq <usrazaq@gmail.com>
2020-05-11 16:28:36 +0100

    added styling and actual model to activities view

---
+++ HelpMyStreetFE/HelpMyStreetFE/Models/RequestHelp/RequestActivityViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HelpMyStreet.Utils.Enums;
+
+namespace HelpMyStreetFE.Models.RequestHelp
+{
+    public class RequestActivityViewModel
+    {
+        public SupportActivities Activity { get; set; }
+        public string FriendlyName { get; set; }
+        public string Description { get; set; }
+        public string Icon { get; set; }
+        public string ColourCode { get; set; }
+    }
+}
