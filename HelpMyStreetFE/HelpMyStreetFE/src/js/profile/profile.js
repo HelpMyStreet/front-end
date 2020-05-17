30d296aa7a5540268f8d3862021b4467cd3887d7
Matthew Law <matthew.law@distinction.co.uk>
2020-05-17 19:12:03 +0100

    Add containers for Jobs

--- HelpMyStreetFE/HelpMyStreetFE/src/js/profile/profile.js
+++ HelpMyStreetFE/HelpMyStreetFE/src/js/profile/profile.js
@@ -1,10 +1,10 @@
 ﻿import { initialiseMyStreet } from "./my-streets";
 import { initialiseVerification } from "./verification";
+import { initialiseRequests } from "./requests";
 
 $(document).ready(function () {
-    
-    initialiseMyStreet();
-    initialiseVerification();
- 
+  initialiseMyStreet();
+  initialiseVerification();
+  initialiseRequests();
 });
-     
+
