d2c345ba7ceb85ec059021350e22b298bb874242
Usmaan Razaq <usrazaq@gmail.com>
2020-05-20 14:47:37 +0100

    added hyper link to accepted requests page

--- HelpMyStreetFE/HelpMyStreetFE/src/js/profile/requests.js
+++ HelpMyStreetFE/HelpMyStreetFE/src/js/profile/requests.js
@@ -47,7 +47,7 @@ export function initialiseRequests() {
     $('.accept-request').click(function (evt) {
         if ($(this).attr("data-accepted") != "true") {
         let popupMessage = '<p>Once you accept a task, you are responsible for working with the person who made the request (and / or the person who needs the help - if it was made on behalf of someone else) to get it done.</p>'
-            + '<p>The request will move from this page to your "My Accepted Requests" page - from where you will be able to access more details about it - including contact details by clicking on "View more info".</p>'
+            + '<p>The request will move from this page to your "<a href="/account/acceptedrequests" target="_blank">My Accepted Requests</a>" page - from where you will be able to access more details about it - including contact details by clicking on "View more info".</p>'
             + "<p>You should call the person in need as soon as you can - they may be depending on you!</p>"
                 + "<p>Thank you for your help!</p>"
 
