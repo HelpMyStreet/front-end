5cbc47fb32d2b267c84737d1557c1d6bd8582188
Geoff Burnhill <geoff@factor-50.co.uk>
2020-06-01 16:18:38 +0100

    Fixed Typo

--- HelpMyStreetFE/HelpMyStreetFE/src/js/shared/cookie-helper.js
+++ HelpMyStreetFE/HelpMyStreetFE/src/js/shared/cookie-helper.js
@@ -17,7 +17,7 @@
         // - description: Description for this cookie type. Displayed in
         //                title attribute.        
         title: 'This website uses cookies',
-        message: ' We use cookies to help make our website usable and to understand how visitors interact with us. More information can be found in our <a href="/privacy-policy" target="_blank">Privacy Policy</a>. <br /> You can change or disalbe cookies at any time by changing your browser settings.',
+        message: ' We use cookies to help make our website usable and to understand how visitors interact with us. More information can be found in our <a href="/privacy-policy" target="_blank">Privacy Policy</a>. <br /> You can change or disable cookies at any time by changing your browser settings.',
         expires: 365,
         delay: 2000,
         moreInfoLabel: '',
