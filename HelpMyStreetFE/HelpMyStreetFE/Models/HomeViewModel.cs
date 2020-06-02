e6b244b6066a3588281749d16c19aa84f7d68ba3
Mike Matthews <mike@factor-50.co.uk>
2020-06-02 17:01:45 +0100

    Removing redundant calls to User Service on home page load

--- HelpMyStreetFE/HelpMyStreetFE/Models/HomeViewModel.cs
+++ HelpMyStreetFE/HelpMyStreetFE/Models/HomeViewModel.cs
@@ -3,9 +3,5 @@
     public class HomeViewModel
     {
         public bool isLoggedIn { get; set; }
-        public int PostCodesCoveredPercentage { get; set; }
-        public int NumStreetChampions { get; set; }
-        public int NumStreetsCovered { get; set; }
-        public int NumVolunteers { get; set; }
     }
 }
