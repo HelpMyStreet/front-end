0bf0fc7fc14f508a04a4230a489732c71bece80a
Usmaan Razaq <usrazaq@gmail.com>
2020-06-08 12:52:16 +0100

    removed S from face coverings friendly name

--- HelpMyStreetFE/HelpMyStreetFE/Helpers/Extensions.cs
+++ HelpMyStreetFE/HelpMyStreetFE/Helpers/Extensions.cs
@@ -17,7 +17,7 @@ namespace HelpMyStreetFE
             return activity switch
             {
                 SupportActivities.Shopping => "Shopping",
-                SupportActivities.FaceMask => "Homemade Face Coverings",
+                SupportActivities.FaceMask => "Homemade Face Covering",
                 SupportActivities.CheckingIn => "Check in",
                 SupportActivities.CollectingPrescriptions => "Prescriptions",
                 SupportActivities.Errands => "Errands",
