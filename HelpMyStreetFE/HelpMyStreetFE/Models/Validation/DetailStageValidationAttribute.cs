d11d26908fcdf0bfeee06054e315f9d9cd63327d
Usmaan Razaq <usrazaq@gmail.com>
2020-06-09 19:58:10 +0100

    server side validation fix for organisation

--- HelpMyStreetFE/HelpMyStreetFE/Models/Validation/DetailStageValidationAttribute.cs
+++ HelpMyStreetFE/HelpMyStreetFE/Models/Validation/DetailStageValidationAttribute.cs
@@ -24,15 +24,13 @@ namespace HelpMyStreetFE.Models.Validation
                     
                 switch (vm.Type)
                 {
-                    case HelpMyStreet.Utils.Enums.RequestorType.Organisation:
+   
                     case HelpMyStreet.Utils.Enums.RequestorType.Myself:
                         ValiadteRecipient(vm, errors, true);
 
-
                        break;
+                    case HelpMyStreet.Utils.Enums.RequestorType.Organisation:
                     case HelpMyStreet.Utils.Enums.RequestorType.OnBehalf:
-                        if (vm.Recipient == null) errors.Add($"Recipient cannot be null");
-                        if (vm.Requestor == null) errors.Add($"Requestor cannot be null");
                         ValiadteRecipient(vm, errors, false);
                         ValidateRequestor(vm, errors);
                         break;                                                                                            
