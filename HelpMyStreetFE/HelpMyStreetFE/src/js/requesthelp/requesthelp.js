2e362c925026e931e8fd5e834f937c0d91df6ac1
Mike Matthews <mike@factor-50.co.uk>
2020-06-16 13:14:34 +0100

    Moving tracking code to separate file.  Adding retry.

--- HelpMyStreetFE/HelpMyStreetFE/src/js/requesthelp/requesthelp.js
+++ HelpMyStreetFE/HelpMyStreetFE/src/js/requesthelp/requesthelp.js
@@ -1,11 +1,12 @@
 ﻿import { intialiseRequestStage,  } from "./request-stage";
 import { initaliseDetailStage,  } from "./detail-stage";
 import { intialiseReviewStage,  } from "./review-stage";
+import { trackPageView } from "../shared/tracking-helper";
 
 
 
 
-$(() => {   
+$(() => {
     switch (stepIndex) {
         case 0:
             intialiseRequestStage();
@@ -16,5 +17,7 @@ $(() => {
         case 2:
             intialiseReviewStage();
             break;
-    }        
+    }
+
+    trackPageView('/request-help/step' + stepIndex);
 });
---
+++ HelpMyStreetFE/HelpMyStreetFE/src/js/shared/tracking-helper.js
@@ -0,0 +1,19 @@
+﻿export function trackPageView(url) {
+    $(window).on('load', function () {
+        submitPageViewToGa(url, 100, 10)
+    });
+}
+
+function submitPageViewToGa(url, retryDelayMs, attemptsToMake) {
+    if (typeof ga !== 'undefined') {
+        ga('set', 'page', url);
+        ga('send', 'pageview');
+    } else {
+        if (attemptsToMake > 0) {
+            console.warn('ga not defined. retrying...');
+            window.setTimeout(submitPageViewToGa(url, retryDelayMs, attemptsToMake - 1), retryDelayMs);
+        } else {
+            console.warn('ga not defined. giving up...');
+        }
+    }
+}
