7085666d70d3584e202b61e9bc863bd9695c3b3c
Mike Matthews <mike@factor-50.co.uk>
2020-06-16 13:55:16 +0100

    refactoring

--- HelpMyStreetFE/HelpMyStreetFE/src/js/shared/tracking-helper.js
+++ HelpMyStreetFE/HelpMyStreetFE/src/js/shared/tracking-helper.js
@@ -1,7 +1,8 @@
-﻿export function trackPageView(url) {
-    $(window).on('load', function () {
-        submitPageViewToGa(url, 100, 10)
-    });
+﻿const gaRetryDelayMs = 250;
+const gaRetryAttempts = 10;
+
+export function trackPageView(url) {
+    submitPageViewToGa(url, gaRetryDelayMs, gaRetryAttempts)
 }
 
 function submitPageViewToGa(url, retryDelayMs, attemptsToMake) {
@@ -10,10 +11,10 @@ function submitPageViewToGa(url, retryDelayMs, attemptsToMake) {
         ga('send', 'pageview');
     } else {
         if (attemptsToMake > 0) {
-            console.warn('ga not defined. retrying...');
+            console.warn('ga not defined; retrying');
             window.setTimeout(submitPageViewToGa(url, retryDelayMs, attemptsToMake - 1), retryDelayMs);
         } else {
-            console.warn('ga not defined. giving up...');
+            console.warn('ga not defined');
         }
     }
 }
