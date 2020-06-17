9bf5e6d56eff9ce5f2305f0fc98503c35b8380f2
Usmaan Razaq <usrazaq@gmail.com>
2020-06-17 15:05:16 +0100

    community page work

--- HelpMyStreetFE/HelpMyStreetFE/Models/Community/CommunityViewModel.cs
+++ HelpMyStreetFE/HelpMyStreetFE/Models/Community/CommunityViewModel.cs
@@ -9,25 +9,49 @@ namespace HelpMyStreetFE.Models.Community
         public double Latitude { get; set; }
         public double Longitude { get; set; }
         public int ZoomLevel { get; set; }
-
+        public string SignUpLink { get; set; } = "/registration/stepone";
         public string CommunityName { get; set; }
 
         public string BannerImageLocation { get; set; }
 
         public string Header { get; set; }
-        public string HeaderHTML { get; set; }
+        public string HeaderHTML { get; set; } = GetDefaultHeaderHtml();
         public string CommunityVolunteersHeader { get; set; }
         public string CommunityVolunteersTextHtml { get; set; }
 
         public bool ShowRequestHelp { get; set; } = true;
         public bool ShowHelpExampleCards {get;set;} = true;
+        public bool DisableButtons { get; set; }
         public string RequestHelpHeading { get; set; }
         public string RequestHelpText { get; set; }
 
         public string ProvideHelpHeading { get; set; }
         public string ProvideHelpText { get; set; }
-
         public IEnumerable<CommunityVolunteer> CommunityVolunteers { get; set; }
         public IEnumerable<string> UsefulLinksHtml { get; set; }
+
+
+        private static string GetDefaultHeaderHtml()
+        {
+            return @"
+                    <p class='row sm12 text-left mt- sm mb-xs'>
+                        HelpMyStreet connects neighbours in need with people who can help -safely and securely.< br >
+                    </p>
+                   <div class='row sm12'>
+                        <ul class='tick-list mt-xs mb-sm compact-list'>
+                            <li>Shopping for essentials</li>
+                            <li>Cooking a hot meal</li>
+                            <li>Home-sewn face coverings</li>
+                            <li>Help with home-schooling</li>
+                            <li>A friendly chat</li>
+                        </ul>
+                    </div>
+                    <p class='row sm12 mt-sm mb-sm'>It's OK to ask for a little help</p>
+                    <div class='row sm12 text-center justify-center small-screen-only mb-sm'>
+                        <a href='/request-help' class='btn cta small btn--request-help cta--orange'>Request Help</a>
+                    </div>
+                    <p class='row sm12 mt-sm mb-sm'>Help your neighbours in need</p>";
+        }
+
     }
 }
--- HelpMyStreetFE/HelpMyStreetFE/Repositories/CommunityRepository.cs
+++ HelpMyStreetFE/HelpMyStreetFE/Repositories/CommunityRepository.cs
@@ -28,25 +28,57 @@ namespace HelpMyStreetFE.Repositories
 
             communityViewModel.CommunityName = "Healthy London";
 
-            communityViewModel.BannerImageLocation = "/img/community/tankersley/tankersley-st-peters-church.jpeg";
+            communityViewModel.BannerImageLocation = "/img/community/hlp/hlp-banner.png";
 
-            communityViewModel.Header = "What are community Connectors";
+            communityViewModel.Header = "What are Community Connectors?";
+            communityViewModel.DisableButtons = true;
+            communityViewModel.SignUpLink = communityViewModel.SignUpLink + "/hlp";
+            communityViewModel.HeaderHTML = @"
+                    <p class='row sm12 text-left mt-sm mb-xs'>
+                        Mental Health First Aid England is working with the new NHS Connect service to recruit volunteer 
+                        Community Connectors as part of the nation-wide response to Covid-19. NHS Connect is a new digital
+                        service that helps connect vulnerable people with the support they need.<br>
+                    </p>
+                    <p class='row sm12 text-left mt-sm mb-xs'>
+                      We are looking for volunteers who combine an understanding of mental health problems with previous training 
+                        and experience in one or more of these practical and ethical frameworks: coaching, Motivational Interviewing,
+                        counselling or an accredited form of therapy.
+                    </p>
+                    <p class='row sm12 text-left mt-sm mb-xs'>This is your opportunity to sign up as a pioneer volunteer</p>
+                   <div class='row input sm12'>
+                        <p class='mb-xs mt-sm'>Please confirm that:</p>
+                       <form>
+                            <div class='input input--checkbox'>
+                                <label class='small'>
+                                    <input type='checkbox' class='select-all' hidden />
+                                    <span class='input--checkbox__checkbox'>
+                                        <span class='mdi mdi-check'></span>
+                                    </span>
+                                    You pledge to commit 3-4 hours or your time per week (although no two weeks are alike) 
+                                </label>
+                            </div>
+                            <div class='input input--checkbox'>
+                                <label class='small'>
+                                    <input type='checkbox' class='select-all' hidden />
+                                    <span class='input--checkbox__checkbox'>
+                                        <span class='mdi mdi-check'></span>
+                                    </span>
+                                    You have previous training and experience in one or more of these practical and ethical frameworks (coaching; motivational interviewing; counselling, or an accredited form of therapy)
+                                </label>
+                            </div>
+                       </form>
+                    </div>";                        
+                        
+                            
             communityViewModel.ShowRequestHelp = false;
             communityViewModel.ShowHelpExampleCards = false;
             communityViewModel.CommunityVolunteersHeader = "Welcome from Healthy London Partnership";
 
 
-
             communityViewModel.CommunityVolunteersTextHtml =
- @"<p>Pilley and Tankersley community helpers are here to help neighbours in need. We can help collecting shopping, running local errands or walking the dog. If you need anything, all you need to do is ask! Request help or sign-up to join us using the buttons below, or call us on XXX.</p>
-<p>Did you know... we also have a community fund to help people struggling to buy the essentials. If you need help paying for your shopping please don't go without - call us on XXXX for a confidential chat, we're here to help.</p>
-<p>If you want to get in touch, email <a href = ""mailto: tankersley@helpmystreet.org"">tankersley@helpmystreet.org</a></p> 
-";
-                        
-
-            communityViewModel.ProvideHelpHeading = "Become a Community Connector";
-
-            communityViewModel.ProvideHelpText = "Join us to help your neighbours. Just let us know when, where and how you can help. You can choose to help a little, or to help a lot! We're grateful for every contribution.";
+            @"<p>We aim to make London the healthiest global city by working with our partners to improve Londoners' health and wellbeing so everyone can live healthier lives.</p>
+            <p>Our partners include the NHS in London (Clinical Commisioning Groups, Health Education England, NHS England, NHS Digital, NHS Improvement, trusts and providers), the Greater London Authority, the Mayor of 
+            London, Public Health England and London Councils.</p>";
 
             communityViewModel.CommunityVolunteers = new List<CommunityVolunteer>()
             {
@@ -90,9 +122,8 @@ namespace HelpMyStreetFE.Repositories
 
             communityViewModel.UsefulLinksHtml = new List<string>()
             {
-                @"<a href=""https://www.healthylondon.org/"">Healthy London Partnership</a>",
-                @"Piley & Tankersley Community Fund - call XXX on XXX for a confidential chat",
-                @"<a href=""https://mhfaengland.org/"">MHFA England</a>",
+                @"<a href=""https://www.healthylondon.org/"">Healthy London Partnership</a> - for more information on the work we do.",                
+                @"<a href=""https://mhfaengland.org/"">MHFA England</a> - to learn more about mental health training",
             };
 
             return communityViewModel;
@@ -111,27 +142,6 @@ namespace HelpMyStreetFE.Repositories
             communityViewModel.BannerImageLocation = "/img/community/tankersley/tankersley-st-peters-church.jpeg";
 
             communityViewModel.Header = "Pilley and Tankersley people taking care of each other";
-
-
-            communityViewModel.HeaderHTML = @"
-                    <p class='row sm12 text - left mt - sm mb - xs'>
-                        HelpMyStreet connects neighbours in need with people who can help -safely and securely.< br >
-                    </p>
-                   <div class='row sm12'>
-                        <ul class='tick-list mt-xs mb-sm compact-list'>
-                            <li>Shopping for essentials</li>
-                            <li>Cooking a hot meal</li>
-                            <li>Home-sewn face coverings</li>
-                            <li>Help with home-schooling</li>
-                            <li>A friendly chat</li>
-                        </ul>
-                    </div>
-                    <p class='row sm12 mt-sm mb-sm'>It's OK to ask for a little help</p>
-                    <div class='row sm12 text-center justify-center small-screen-only mb-sm'>
-                        <a href='/request-help' class='btn cta small btn--request-help cta--orange'>Request Help</a>
-                    </div>
-                    <p class='row sm12 mt-sm mb-sm'>Help your neighbours in need</p>";
-
 
             communityViewModel.CommunityVolunteersHeader = "Welcome from Pilley and Tankersley Community Helpers";
 
