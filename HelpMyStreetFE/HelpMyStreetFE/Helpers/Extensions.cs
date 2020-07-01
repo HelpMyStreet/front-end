f7251e0c9efe67bb447159bfaf4275482714fbff
Mike Matthews <mike@factor-50.co.uk>
2020-07-01 16:55:55 +0100

    FTLOS-3 Adding max face covering quantity and associated messaging

--- HelpMyStreetFE/HelpMyStreetFE/Helpers/Extensions.cs
+++ HelpMyStreetFE/HelpMyStreetFE/Helpers/Extensions.cs
@@ -168,6 +168,24 @@ namespace HelpMyStreetFE
                 _ => string.Empty
             };
         }
+
+        public static int? Max(this RequestHelpQuestion question)
+        {
+            return question.ID switch
+            {
+                (int)Questions.FaceMask_Amount => 200,
+                _ => null
+            };
+        }
+
+        public static string DataValidationMessage(this RequestHelpQuestion question)
+        {
+            return question.ID switch
+            {
+                (int)Questions.FaceMask_Amount => @"Please enter a number between 1 and 200. We can only accept requests for up to 200 face coverings using this form, if you need more email For the Love of Scrubs at fortheloveofscrubs@outlook.com",
+                _ => question.DataValidationMessage
+            };
+        }
     }
 
 
