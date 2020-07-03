74d5926acbe6021cba0bfde463466cd1d7c681a5
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-07-03 13:02:12 +0100

    Add connectivity to group service

---
+++ HelpMyStreetFE/HelpMyStreetFE/Services/IGroupService.cs
@@ -0,0 +1,12 @@
+﻿using HelpMyStreet.Contracts.GroupService.Response;
+using HelpMyStreet.Contracts.RequestService.Response;
+using HelpMyStreet.Contracts.Shared;
+using System.Threading.Tasks;
+
+namespace HelpMyStreetFE.Services
+{
+    public interface IGroupService
+    {
+        Task<ResponseWrapper<GetGroupByKeyResponse, GroupServiceErrorCode>> GetGroupByKey(string groupKey); 
+    }
+}
