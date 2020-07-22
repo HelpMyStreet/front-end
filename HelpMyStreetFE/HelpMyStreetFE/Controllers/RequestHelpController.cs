532610a23bdbd904766ad73b3ca297dee79beffb
Mike Matthews <mike@factor-50.co.uk>
2020-07-22 11:01:26 +0100

    Refactoring

--- HelpMyStreetFE/HelpMyStreetFE/Controllers/RequestHelpController.cs
+++ HelpMyStreetFE/HelpMyStreetFE/Controllers/RequestHelpController.cs
@@ -219,10 +219,11 @@ namespace HelpMyStreetFE.Controllers
 
             string message = "<p>Your request has been received and we are looking for a volunteer who can help. Someone should get in touch shortly.</p>";
 
-            string link = User.Identity.IsAuthenticated ? "/account" : "/";
+            string doneLink = User.Identity.IsAuthenticated ? "/account" : "/";
+            string button = $" <a href='{doneLink}' class='btn cta large fill mt16 btn--request-help cta--orange'>Done</a>";
 
-            string button = $" <a href='{link}' class='btn cta large fill mt16 btn--request-help cta--orange'>Done</a>";
-            string requestLink = "/request-help/" + Base64Utils.Base64Encode(referringGroupId.ToString()) + "/" + source;
+            string encodedReferringGroupId = Base64Utils.Base64Encode(referringGroupId.ToString());
+            string requestLink = $"/request-help/{encodedReferringGroupId}/{source}";
 
             string facemaskmessage = "<p>For the Love of Scrubs ask for a small donation of £3 - £4 per face covering to cover the cost of materials and help support their communities. Without donations they aren’t able to continue their good work.</p>" +
                 "<p>If you are able to donate, you can do so on their Go Fund Me page <a href=\"https://www.gofundme.com/f/for-the-love-of-scrubs-face-coverings\" target=\"_blank\">here</a>.<p>";
