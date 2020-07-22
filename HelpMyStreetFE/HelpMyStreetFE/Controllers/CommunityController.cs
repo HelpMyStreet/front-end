425ceb565a94fec87f126a2ab15797d6c2ab5dd8
Mike Matthews <mike@factor-50.co.uk>
2020-07-22 08:59:24 +0100

    FTLOS-97 tidy

--- HelpMyStreetFE/HelpMyStreetFE/Controllers/CommunityController.cs
+++ HelpMyStreetFE/HelpMyStreetFE/Controllers/CommunityController.cs
@@ -79,8 +79,6 @@ namespace HelpMyStreetFE.Controllers
 
         public async Task<IActionResult> FaceCoverings()
         {
-            var ftlosGetGroupByKeyResponse = await _groupService.GetGroupByKey("ftlos");
-            var encodedFtlosGroupId = Base64Utils.Base64Encode(ftlosGetGroupByKeyResponse.GroupId.ToString());
             var genericGetGroupByKeyResponse = await _groupService.GetGroupByKey("Generic");
             var encodedGenericGroupId = Base64Utils.Base64Encode(genericGetGroupByKeyResponse.GroupId.ToString());
 
