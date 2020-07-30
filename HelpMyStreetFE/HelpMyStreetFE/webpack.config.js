98a3b1d930b5f36966b909d69f9c86582e247105
Mike Matthews <mike@factor-50.co.uk>
2020-07-30 15:53:17 +0100

    Display performance warnings in webpack

--- HelpMyStreetFE/HelpMyStreetFE/webpack.config.js
+++ HelpMyStreetFE/HelpMyStreetFE/webpack.config.js
@@ -6,6 +6,9 @@ module.exports = {
     minimize: true,
     minimizer: [new TerserPlugin()],
   },
+  performance: {
+    hints: 'warning'
+  },
   context: path.resolve(__dirname, "src"),
   entry: {
     main: "./js/app.js",
