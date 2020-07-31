12c2f73775f362a14ee46c1a621b3bcde9f32ac9
Mike Matthews <mike@factor-50.co.uk>
2020-07-31 12:14:42 +0100

    Adding secure and samesite attributes to cookies

--- HelpMyStreetFE/HelpMyStreetFE/wwwroot/lib/GDPRCookie/jquery.ihavecookies.custom.js
+++ HelpMyStreetFE/HelpMyStreetFE/wwwroot/lib/GDPRCookie/jquery.ihavecookies.custom.js
@@ -198,7 +198,7 @@
         var d = new Date();
         d.setTime(d.getTime() + (expiry_days * 24 * 60 * 60 * 1000));
         var expires = "expires=" + d.toUTCString();
-        document.cookie = name + "=" + value + ";" + expires + ";path=/";
+        document.cookie = name + "=" + value + ";" + expires + ";path=/;secure;samesite=strict";
         return getCookie(name);
     };
 
