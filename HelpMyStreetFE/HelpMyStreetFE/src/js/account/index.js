eaa4482ddc1fa2c55621e15991b131c6b388c3b6
Mike Matthews <mike@factor-50.co.uk>
2020-08-04 15:14:19 +0100

    Adding menu expanders

--- HelpMyStreetFE/HelpMyStreetFE/src/js/account/index.js
+++ HelpMyStreetFE/HelpMyStreetFE/src/js/account/index.js
@@ -2,3 +2,16 @@ import login from "./login";
 import notification from "./notification";
 
 export default { login, notification };
+
+$(document).ready(function () {
+    initialiseAccountNameExpanders();
+});
+
+function initialiseAccountNameExpanders() {
+    $('.account__nav .account__nav__item.submenu__container').each(function () {
+        $(this).find('a').first().on('click', function (event) {
+            $(this).next('ul').slideToggle();
+            event.preventDefault();
+        });
+    });
+}
