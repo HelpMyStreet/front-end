7c35891a93f42aec040547d124b164eb73ead947
Mike Matthews <mike@factor-50.co.uk>
2020-08-04 12:08:12 +0100

    Adding models for UserGroups

---
+++ HelpMyStreetFE/HelpMyStreetFE/Models/Account/GroupViewModel.cs
@@ -0,0 +1,16 @@
+﻿using HelpMyStreet.Utils.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelpMyStreetFE.Models.Account
+{
+    public class GroupViewModel
+    {
+        public int GroupId { get; set; }
+        public string GroupName { get; set; }
+        public string GroupKep { get; set; }
+        public List<GroupRoles> UserRoles { get; set; }
+    }
+}
---
+++ HelpMyStreetFE/HelpMyStreetFE/Models/Account/UserGroups.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace HelpMyStreetFE.Models.Account
+{
+    public class UserGroups
+    {
+        public List<GroupViewModel> Groups { get; set; }
+    }
+}
