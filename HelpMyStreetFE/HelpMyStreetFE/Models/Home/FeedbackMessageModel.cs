20ce5e14280dfda62e954f201bf47d57282c5db7
Geoff Burnhill <geoff@factor-50.co.uk>
2020-08-05 16:37:28 +0100

    Update to reduce FaceMask obsession

--- HelpMyStreetFE/HelpMyStreetFE/Models/Home/FeedbackMessageModel.cs
+++ HelpMyStreetFE/HelpMyStreetFE/Models/Home/FeedbackMessageModel.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 namespace HelpMyStreetFE.Models.Home
 {
+    public enum MessageType
+    {
+        FaceCovering,
+        Group,
+        Other
+    }
     public class FeedbackMessage
     {
         public string Tagline { get; set; }
         public string Message { get; set; }
         public string Person { get; set; }
+        public MessageType Type { get; set; }
     }
 }
