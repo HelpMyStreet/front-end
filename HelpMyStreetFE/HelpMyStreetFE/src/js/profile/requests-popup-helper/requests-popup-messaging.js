87260181cd21de15d8db51531b4ace7706f99dcb
Mike Matthews <mike@factor-50.co.uk>
2020-08-12 12:12:45 +0100

    Notification popup wording changes

--- HelpMyStreetFE/HelpMyStreetFE/src/js/profile/requests-popup-helper/requests-popup-messaging.js
+++ HelpMyStreetFE/HelpMyStreetFE/src/js/profile/requests-popup-helper/requests-popup-messaging.js
@@ -8,15 +8,18 @@
             settings.htmlContent =
                 `<p>It will appear on your "My Accepted Requests" page and you’ll be able to view more information about it.</p>
                  <p>Please use the information in the request to fulfil it as soon as possible (this may involve contacting the recipient) – the ball’s in your court, someone may be depending on you.</p>
+		         <p>The requester will be notified that their request has been accepted, but won't be given your contact details.</p>
                  <p>Thank you for helping people in your community to stay safe.</p>`;
             settings.messageOnFalse = "We couldn't accept this request at the moment, please try again later";
-            settings.actionBtnText = "Accept";
 
         } else if (currentState === "Done") {
 
-            settings.header = "Mark as In Progress";
+            settings.header = "Mark as In Progress?";
             settings.htmlContent =
-                `<p>It will return to the volunteer's "My Accepted Requests" page.</p>`;
+                `<p>Have you become aware that this request has not been completed (though it has been marked as such)?</p>
+		         <p>We can set its status back to "In Progress" for you.</p>
+		         <p>It will return to the "My Accepted Requests" page of the last volunteer with who it was In Progress.</p>
+		         <p>If you need to set it right back to "Open", you can do that from this page by exiting and returning to it, finding the request again (whcih will now be marked as "In Progress") and using the "Mark as Open" button.`;
             settings.messageOnFalse = "We couldn't update this request at the moment, please try again later";
 
         }
@@ -26,12 +29,13 @@
             settings.header = "Completed?";
             if (userActingAsAdmin) {
                 settings.htmlContent =
-                    `<p>We’ll mark this request as complete.</p>
-                     <p>Thank you for helping people in your community stay safe.</p>`;
+                    `<p>Have you become aware that this task has been completed, but not marked as such?</p>
+                     <p>We can mark it as mark it as Complete on behalf of the volunteer for you.</p>`;
             } else {
                 settings.htmlContent =
-                    `<p>We’ll move this request from your Accepted Requests page.</p>
-                     <p>Thank you for helping people in your community stay safe.</p>`;
+                    `<p>Thank you so much for helping people in your community stay safe!</p>
+		             <p>We’ll remove this request from your "My Accepted Requests" page.</p>
+                     <p>The requester will be notified that the request has been marked as Complete.</p>`;
             }
             settings.messageOnFalse = "We couldn't complete this request at the moment, please try again later";
 
@@ -40,16 +44,18 @@
         if (currentState === "InProgress") {
 
             if (userActingAsAdmin) {
-                settings.header = "Mark as open?";
-                settings.htmlContent = 
-                    `<p>Request unable to be completed?</p>
-                     <p>We can return it to the 'Open Requests' list so that other volunteers can accept it.</p>`;
+                settings.header = "Mark as Open?";
+                settings.htmlContent =
+                    `<p>Have you become aware that the volunteer who accepted this request is unable to complete it?</p>
+                     <p>We can return it to the "Open Requests" list so that other volunteers can accept it.</p>
+		             <p>If there's a problem with the request (e.g. incorrect contact details) please let us know: 
+		             <a href="mailto:support@helpmystreet.org">support@helpmystreet.org</a></p>`;
             } else {
                 settings.header = "Can't do?";
-                settings.htmlContent = 
+                settings.htmlContent =
                     `<p>Not able to complete the request?</p>
                      <p>If you're not able to complete a request for help, we can return it to the 'Open Requests' list so that other volunteers can accept it.</p>
-                     <p>If there's a problem (e.g. incorrect contact details) please let us know:
+                     <p>If there's a problem with the request (e.g. incorrect contact details) please let us know:
                      <a href="mailto:requests@helpmystreet.org">requests@helpmystreet.org</a></p>`;
             }
             settings.messageOnFalse = "We couldn't release this request at the moment, please try again later";
@@ -59,23 +65,23 @@
         if (currentState === "Open") {
 
             settings.header = "Cancel?";
-            settings.htmlContent = 
-                `<p>Request is no longer current?</p>
-                 <p>We can mark is as cancelled and remove it from volunteers' Open Requests view.</p>`;
+            settings.htmlContent =
+                `<p>Is there a problem with this request (e.g. you have become aware that it was entered in error or that the help is no longer needed)?</p>
+                 <p>We can mark is as Cancelled and remove it from the "Open Requests" list.</p>`;
 
         } else if (currentState === "InProgress") {
 
-            settings.header = "Mark as cancelled?";
-            settings.htmlContent = 
-                `<p>Request is no longer current?</p>
-                 <p>We can mark is as cancelled and remove it from the volunteer's Accepted Requests view.</p>`;
+            settings.header = "Cancel?";
+            settings.htmlContent =
+                `<p>Is there a problem with this request (e.g. you have become aware that it was entered in error or that the help is no longer needed)?</p>
+                 <p>We can mark is as Cancelled and remove it from the relevant volunteer's "My Accepted Requests" page.</p>`;
 
         } else if (currentState === "Done") {
 
             settings.header = "Mark as cancelled?";
-            settings.htmlContent = 
-                `<p>Request was not genuine?</p>
-                 <p>We can mark it as cancelled.</p>`;
+            settings.htmlContent =
+                `<p>Is there a problem with this request (e.g. you have become aware that it was entered in error or that the help is no longer needed)?</p>
+                 <p>We can mark is as Cancelled for your records.</p>`;
 
         }
         settings.messageOnFalse = "We couldn't cancel this request at the moment, please try again later";
