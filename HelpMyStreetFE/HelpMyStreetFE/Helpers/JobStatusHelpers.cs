b43d99daa913f897d5327eeefff63f97ea672fed
Mike Matthews <mike@factor-50.co.uk>
2020-08-14 15:55:44 +0100

    Adding FriendlyName() for JobStatus

---
+++ HelpMyStreetFE/HelpMyStreetFE/Helpers/JobStatusHelpers.cs
@@ -0,0 +1,16 @@
+﻿using HelpMyStreet.Utils.Enums;
+
+namespace HelpMyStreetFE
+{
+    public static class JobStatusHelpers
+    {
+        public static string FriendlyName(this JobStatuses status)
+        {
+            return status switch
+            {
+                JobStatuses.InProgress => "In Progress",
+                _ => status.ToString()
+            };
+        }
+    }
+}
