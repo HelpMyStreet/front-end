8b0c9c3743c7d552840fb8c960892a3b2589253c
Mike Matthews <mike@factor-50.co.uk>
2020-08-26 15:56:44 +0100

    Moving results panel into JobFilterPanel

--- HelpMyStreetFE/HelpMyStreetFE/Models/Account/Jobs/JobFilterViewModel.cs
+++ HelpMyStreetFE/HelpMyStreetFE/Models/Account/Jobs/JobFilterViewModel.cs
@@ -1,14 +1,18 @@
 ﻿using HelpMyStreet.Utils.Enums;
+using HelpMyStreetFE.Enums.Account;
 using System.Collections.Generic;
-using System.Linq;
+using System;
 
 namespace HelpMyStreetFE.Models.Account.Jobs
 {
     public class JobFilterViewModel
     {
+        public JobSet JobSet { get; set; }
+        public int GroupId { get; set; }
         public IEnumerable<FilterField<SupportActivities>> SupportActivities { get; set; }
         public IEnumerable<FilterField<JobStatuses>> JobStatuses { get; set; }
         public IEnumerable<FilterField<int>> MaxDistanceInMiles { get; set; }
         public IEnumerable<FilterField<int>> DueInNextXDays { get; set; }
+        public Action EmptyJobSetCallback { get; set; }
     }
 }
