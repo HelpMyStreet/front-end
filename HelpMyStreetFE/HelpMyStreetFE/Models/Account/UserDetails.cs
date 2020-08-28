670b4d4501742db5c9dbb2fd9d1357ced1b0e091
Mike Matthews <mike@factor-50.co.uk>
2020-08-28 12:07:51 +0100

    Simplifying UserDetails

--- HelpMyStreetFE/HelpMyStreetFE/Models/Account/UserDetails.cs
+++ HelpMyStreetFE/HelpMyStreetFE/Models/Account/UserDetails.cs
@@ -3,58 +3,45 @@ using System.Collections.Generic;
 
 namespace HelpMyStreetFE.Models.Account
 {
-    public class UserDetails //I expect this class to be depricated when we know how to differentiate users
+    public class UserDetails
     {
-        public UserDetails(
-            User user,
-            string initials,
-            string displayName,
-            string firstName,
-            string lastName,
-            string emailAddress,
-            string address,
-            string streetChampion,
-            string mobileNumber,
-            string otherNumber,
-            string dateOfBirth,
-            string gender,    
-            List<string> championPostcodes,
-            bool isStreetChampion,
-            bool isVerified
-            )
+        public UserDetails(User user)
         {
             User = user;
-            Initials = initials;
-            DisplayName = displayName;
-            FirstName = firstName;
-            LastName = lastName;
-            EmailAddress = emailAddress;
-            MobileNumber = mobileNumber;
-            OtherNumber = otherNumber;
-            DateOfBirth = dateOfBirth;
-            Gender = gender;
-            Address = address;
-            StreetChampion = streetChampion;   
-            ChampionPostcodes = championPostcodes;
-            IsStreetChampion = isStreetChampion;
-            IsVerified = isVerified;
         }
 
         public User User { get; set; }
-        public string Initials { get; set; }
-        public string DisplayName { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string EmailAddress { get; set; }
-        public string Address { get; set; }
-        public string MobileNumber { get; set; }
-        public string OtherNumber { get; set; }
-        public string DateOfBirth { get; set; }
-        public string Gender { get; set; }
-        public string StreetChampion { get; set; }
-        public List<string> ChampionPostcodes { get; set; }
-        public bool IsStreetChampion { get; set; }
-        public bool IsVerified { get; set; }
+        public string Initials
+        {
+            get
+            {
+                if (User?.UserPersonalDetails == null || string.IsNullOrEmpty(User.UserPersonalDetails.FirstName))
+                {
+                    return "??";
+                }
+                return User.UserPersonalDetails.FirstName.Substring(0, 1).ToUpper() + User.UserPersonalDetails.LastName.Substring(0, 1).ToUpper();
+            }
+        }
+        public string DisplayName { get { return User?.UserPersonalDetails?.DisplayName ?? "??"; } }
+        public string FirstName { get { return User?.UserPersonalDetails?.FirstName ?? "Not Set"; } }
+        public string LastName { get { return User?.UserPersonalDetails?.LastName ?? "Not Set"; } }
+        public string EmailAddress { get { return User?.UserPersonalDetails?.EmailAddress ?? "Not Set"; } }
+        public string Address { get
+            {
+                if (User?.UserPersonalDetails == null || string.IsNullOrEmpty(User.UserPersonalDetails.Address?.AddressLine1))
+                {
+                    return "Not Set";
+                }
+                return string.Join(", ", new[] { User.UserPersonalDetails.Address.AddressLine1, User.UserPersonalDetails.Address.Postcode });
+            }
+        }
+        public string MobileNumber { get { return User?.UserPersonalDetails?.MobilePhone ?? "Not Set"; } }
+        public string OtherNumber { get { return User?.UserPersonalDetails?.OtherPhone ?? "Not Set"; } }
+        public string DateOfBirth { get { return User?.UserPersonalDetails?.DateOfBirth?.ToString("dd/MM/yyyy") ?? "Not Set"; } }
+        public string StreetChampion { get { return IsStreetChampion ? "Street Champion" : "Helper"; } }
+        public List<string> ChampionPostcodes { get { return User?.ChampionPostcodes ?? new List<string>(); } }
+        public bool IsStreetChampion { get { return User != null && User.StreetChampionRoleUnderstood.HasValue && User.StreetChampionRoleUnderstood.Value; } }
+        public bool IsVerified { get { return User?.IsVerified ?? false; } }
 
     }
 }
