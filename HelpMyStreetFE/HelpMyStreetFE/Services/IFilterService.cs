fa3865d25569b0151d50624a2bf8e5473bf86381
Mike Matthews <mike@factor-50.co.uk>
2020-09-03 11:14:33 +0100

    Moving DefaultFilterSet generation to new FilterService

---
+++ HelpMyStreetFE/HelpMyStreetFE/Services/IFilterService.cs
@@ -0,0 +1,11 @@
+﻿using HelpMyStreet.Utils.Models;
+using HelpMyStreetFE.Enums.Account;
+using HelpMyStreetFE.Models.Account.Jobs;
+
+namespace HelpMyStreetFE.Services
+{
+    public interface IFilterService
+    {
+        public FilterSet GetDefaultFilterSet(JobSet jobSet, User user);
+    }
+}
--- HelpMyStreetFE/HelpMyStreetFE/ViewComponents/JobFilterPanelViewComponent.cs
+++ HelpMyStreetFE/HelpMyStreetFE/ViewComponents/JobFilterPanelViewComponent.cs
@@ -1,16 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using HelpMyStreetFE.Models.Account.Jobs;
+using HelpMyStreetFE.Services;
 
 namespace HelpMyStreetFE.ViewComponents
 {
     public class JobFilterPanelViewComponent : ViewComponent
     {
+        private readonly IFilterService _filterService;
+
+        public JobFilterPanelViewComponent(IFilterService filterService)
+        {
+            _filterService = filterService;
+        }
+
         public async Task<IViewComponentResult> InvokeAsync(JobFilterViewModel jobFilterViewModel)
         {
+            if (jobFilterViewModel.FilterSet == null)
+            {
+                jobFilterViewModel.FilterSet = _filterService.GetDefaultFilterSet(jobFilterViewModel.JobFilterRequest.JobSet, jobFilterViewModel.User);
+            }
+
             return View("JobFilterPanel", jobFilterViewModel);
         }
 
