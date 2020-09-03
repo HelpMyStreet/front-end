f8d4b0f7ea7a79a5def84987ad7d743694e5deff
Mike Matthews <mike@factor-50.co.uk>
2020-09-03 13:57:24 +0100

    Fixing display of next step and job status

--- HelpMyStreetFE/HelpMyStreetFE/src/js/profile/requests.js
+++ HelpMyStreetFE/HelpMyStreetFE/src/js/profile/requests.js
@@ -22,109 +22,110 @@ import {
 } from "../shared/hmsFetch";
 
 export function initialiseRequests(isVerified) {
-    const job = getParameterByName("j");  
+  const job = getParameterByName("j");
 
-    if (job) {
-        var jobEl = $(`#${job}`);
-        if (jobEl.length) {
-            $("html, body").animate(
-                {
-                    scrollTop: jobEl.offset().top,
-                },
-                {
-                    duration: 1000,
-                    complete: () => {
-                        if (isVerified) {
-                            $(`#${job} .job__detail`).slideDown();
-                            $(`#${job}`).addClass("open highlight");
-                            loadJobDetails(jobEl);
-                        } else {
-                            $(`#${job}`).addClass("highlight");
-                        }
-                    },
-                }
-            );
+  if (job) {
+    var jobEl = $(`#${job}`);
+    if (jobEl.length) {
+      $("html, body").animate(
+        {
+          scrollTop: jobEl.offset().top,
+        },
+        {
+          duration: 1000,
+          complete: () => {
+            if (isVerified) {
+              $(`#${job} .job__detail`).slideDown();
+              $(`#${job}`).addClass("open highlight");
+              loadJobDetails(jobEl);
+            } else {
+              $(`#${job}`).addClass("highlight");
+            }
+          },
         }
+      );
     }
+  }
 
-    $('.job-list').on('mouseover', '.job', function () {
-        loadJobDetails($(this));
-    });
+  $('.job-list').on('mouseover', '.job', function () {
+    loadJobDetails($(this));
+  });
 
-    $('.job-list').on('click', '.job a.open', function (e) {
-        e.preventDefault();
-        const job = $(this).closest('.job');
-        if (isVerified) {
-            updateQueryStringParam('j', $(job).attr('id'));
-            job.toggleClass('open');
-            job.find('.job__detail').slideToggle();
-            loadJobDetails(job);
-        } else {
-            showUnVerifiedAcceptPopup();
-        }
-    });
+  $('.job-list').on('click', '.job a.open', function (e) {
+    e.preventDefault();
+    const job = $(this).closest('.job');
+    if (isVerified) {
+      updateQueryStringParam('j', $(job).attr('id'));
+      job.toggleClass('open');
+      job.find('.job__detail').slideToggle();
+      loadJobDetails(job);
+    } else {
+      showUnVerifiedAcceptPopup();
+    }
+  });
 
-    $('.job-list').on('click', '.job a.close', function (e) {
-        e.preventDefault();
-        const job = $(this).closest('.job');
-        removeQueryStringParam('j', $(job).attr('id'));
-        job.toggleClass('open');
-        job.find('.job__detail').slideToggle();
-    });
+  $('.job-list').on('click', '.job a.close', function (e) {
+    e.preventDefault();
+    const job = $(this).closest('.job');
+    removeQueryStringParam('j', $(job).attr('id'));
+    job.toggleClass('open');
+    job.find('.job__detail').slideToggle();
+  });
 
-    $('.job-list').on('click', '.job__expander h4', function (e) {
-        e.preventDefault();
-        $(this).toggleClass('open');
-        $(this).next().slideToggle();
-    });
+  $('.job-list').on('click', '.job__expander h4', function (e) {
+    e.preventDefault();
+    $(this).toggleClass('open');
+    $(this).next().slideToggle();
+  });
 
-    $('.job-list').on('click', '.job button.trigger-status-update-popup', function () {
-        showStatusUpdatePopup($(this));
-    });
+  $('.job-list').on('click', '.job button.trigger-status-update-popup', function () {
+    showStatusUpdatePopup($(this));
+  });
 
-    $('.job-list').on('click', '.accept-request-unverified', function () {
-        showUnVerifiedAcceptPopup();
-    });
+  $('.job-list').on('click', '.accept-request-unverified', function () {
+    showUnVerifiedAcceptPopup();
+  });
 
-    $('.job-list').on('click', '.undo-request', async function (evt) {
-        const job = $(this).closest(".job");
-        const targetState = $(this).data("target-state");
-        const targetUser = $(this).data("target-user") ?? "";
+  $('.job-list').on('click', '.undo-request', async function (evt) {
+    const job = $(this).closest(".job");
+    const targetState = $(this).data("target-state");
+    const targetUser = $(this).data("target-user") ?? "";
 
-        buttonLoad($(this));
-        let newStatus = await setJobStatus(job, targetState, targetUser);
-        if (newStatus) {
-            $(job).find('.job__status').html(newStatus);
-            $(job).find('.job__info__urgency>*').show();
-            $(job).find('button').toggle();
-        }
-        buttonUnload($(this));
-    })
+    buttonLoad($(this));
+    let newStatus = await setJobStatus(job, targetState, targetUser);
+    if (newStatus) {
+      $(job).find('.job__status__new').html('');
+      $(job).find('.job__info__urgency__dates').toggle();
+      $(job).find('button').toggle();
+      $(job).find('.next-step').toggle();
+    }
+    buttonUnload($(this));
+  });
 }
 
 
 
 export function showStatusUpdatePopup(btn) {
-    const job = btn.closest(".job");
-    const targetState = $(btn).data("target-state");
-    const targetUser = $(btn).data("target-user") ?? "self";
+  const job = btn.closest(".job");
+  const targetState = $(btn).data("target-state");
+  const targetUser = $(btn).data("target-user") ?? "self";
 
-    let popupSettings = getPopupMessaging($(job).data("job-status"), targetState, $(job).data("user-acting-as-admin") === "True");
+  let popupSettings = getPopupMessaging($(job).data("job-status"), targetState, $(job).data("user-acting-as-admin") === "True");
 
-    popupSettings.acceptCallbackAsync = async () => {
-        let newStatus = await setJobStatus(job, targetState, targetUser);
+  popupSettings.acceptCallbackAsync = async () => {
+    let newStatus = await setJobStatus(job, targetState, targetUser);
 
-        if (newStatus) {
-            $(job).find('.job__status').html(newStatus);
-            $(job).find('.job__info__urgency>*').hide();
-            $(job).find('button').toggle();
-            $(job).find('.next-step').toggle();
-            return true;
-        }
-        return success;
+    if (newStatus) {
+      $(job).find('.job__status__new').html(newStatus);
+      $(job).find('.job__info__urgency__dates').toggle();
+      $(job).find('button').toggle();
+      $(job).find('.next-step').toggle();
+      return true;
     }
+    return success;
+  };
 
-    showPopup(popupSettings);
+  showPopup(popupSettings);
 }
 
 
