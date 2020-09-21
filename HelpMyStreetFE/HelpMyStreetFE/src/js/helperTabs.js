e086922692c28759396f7453b3d90e6795af4ac9
Geoff Burnhill <geoff@factor-50.co.uk>
2020-09-21 14:33:26 +0100

    Initial commit

---
+++ HelpMyStreetFE/HelpMyStreetFE/src/js/helperTabs.js
@@ -0,0 +1,27 @@
+﻿import $ from "jquery"
+
+$(function () {
+    console.log("loaded");
+
+$(".tablinks").click(function (){
+    console.log("click fired");
+    var tablinks = document.getElementsByClassName("tablinks");
+    var tabcontent = document.getElementsByClassName("tabcontent");
+
+    tablinks.forEach(tab => {
+        tab.className.replace(" active", "");
+    })
+
+    $(this).addClass("active");
+
+    tabcontent.forEach(tab => {
+        if (tabcontent.id == $(this).id + "-content") {
+            tab.className.replace("inactive", "active");
+
+        }
+        else {
+            tab.className.replace("active", "inactive");
+        }
+    })
+})
+})
