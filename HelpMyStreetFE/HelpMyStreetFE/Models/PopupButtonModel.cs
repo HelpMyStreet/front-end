dc78a8e39d57af5534107ff16405ae2a1d86b9a5
Mike Matthews <mike@factor-50.co.uk>
2020-10-13 16:12:46 +0100

    Adding _PopupAcceptButton partial

---
+++ HelpMyStreetFE/HelpMyStreetFE/Models/PopupButtonModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelpMyStreetFE.Models
+{
+    public class PopupButtonModel
+    {
+        public string ButtonText { get; set; }
+    }
+}
