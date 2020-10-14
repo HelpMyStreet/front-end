9acc37696134b49cbe1145e0e1c1e073b3d6e68a
Mike Matthews <mike@factor-50.co.uk>
2020-10-14 17:36:00 +0100

    Expander!

--- HelpMyStreetFE/HelpMyStreetFE/src/js/profile/profile.js
+++ HelpMyStreetFE/HelpMyStreetFE/src/js/profile/profile.js
@@ -1,11 +1,13 @@
 ﻿import { initialiseMyStreet } from "./my-streets";
 import { initialiseVerification } from "./verification";
 import { initialiseRequests } from "./requests";
+import { initialiseGenericExpanders } from "../ui/generic-expander";
 
 $(document).ready(function () {
-    const isVerified = (initObj && initObj.isVerified == "True");
-    initialiseMyStreet();
-    initialiseVerification(isVerified);
-    initialiseRequests(isVerified);
+  const isVerified = (initObj && initObj.isVerified == "True");
+  initialiseMyStreet();
+  initialiseVerification(isVerified);
+  initialiseRequests(isVerified);
+  initialiseGenericExpanders();
 });
 
---
+++ HelpMyStreetFE/HelpMyStreetFE/src/js/ui/generic-expander.js
@@ -0,0 +1,8 @@
+﻿export function initialiseGenericExpanders() {
+  $('body').on('click', 'a.generic-expander__control', function (e) {
+    e.preventDefault();
+    const expander = $(this).closest('.generic-expander');
+    expander.toggleClass('open');
+    expander.find('.generic-expander__content').slideToggle();
+  });
+}
