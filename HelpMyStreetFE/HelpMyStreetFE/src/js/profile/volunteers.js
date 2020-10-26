10591796d4dcdf9a647cb38ad5746c62da04f9cd
Mike Matthews <mike@factor-50.co.uk>
2020-10-26 21:17:29 +0000

    FE-461 Adding validation

--- HelpMyStreetFE/HelpMyStreetFE/src/js/profile/volunteers.js
+++ HelpMyStreetFE/HelpMyStreetFE/src/js/profile/volunteers.js
@@ -1,6 +1,7 @@
 ﻿import { showServerSidePopup } from '../shared/popup';
 import { hmsFetch, fetchResponses } from '../shared/hmsFetch';
 import { datepickerLoad, validateDate, dateValidationSchemes } from '../shared/date-picker';
+import { validateFormData } from '../shared/validator'
 
 export function initialiseVolunteerList() {
   initialiseAddCredentialLinks();
@@ -20,7 +21,7 @@ var initialiseAddCredentialLinks = function () {
     let popup;
     const settings = {
       acceptCallbackAsync: async () => {
-        if (!validateCredentialForm()) {
+        if (!validateCredentialForm($(popup).find('form'))) {
           return 'Please check your entries above and try again.';
         }
 
@@ -78,8 +79,12 @@ var intialiseCredentialPopupTiles = function () {
   });
 };
 
-var validateCredentialForm = function () {
+var validateCredentialForm = function (form) {
   $('#datepicker-error').hide();
+  const valid = validateFormData(form, {
+    Reference: (v) => v.length < 100 || "Reference number must be fewer than 100 charachters in length",
+    Notes: (v) => v.length < 200 || "Reference number must be fewer than 200 charachters in length",
+  });
   const dateValue = $('input[name="ValidUntil"]').val();
   if (dateValue.length == 0) {
     $('#datepicker-error').text('Please select an option or enter a date').show();
@@ -88,5 +93,5 @@ var validateCredentialForm = function () {
   if (dateValue !== 'Null' && !validateDate(dateValue, 'datepicker', 'datepicker-error', dateValidationSchemes.FUTURE_DATES)) {
     return false;
   }
-  return true;
+  return valid;
 };
