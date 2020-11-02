1a22ac7d44c0d57f0fdbbebe2cc12cb3ea35e3fb
Mike Matthews <mike@factor-50.co.uk>
2020-11-02 13:35:44 +0000

    Comma

--- HelpMyStreetFE/HelpMyStreetFE/Repositories/CommunityRepository.cs
+++ HelpMyStreetFE/HelpMyStreetFE/Repositories/CommunityRepository.cs
@@ -18,7 +18,7 @@ namespace HelpMyStreetFE.Repositories
             {"tankersley", new CommunityModel(){FriendlyName = "Tankersley & Pilley Community Helpers", Latitude = 53.498113, Longitude = -1.488587, ReferenceName = "tankersley", LinkURL = "/tankersley", ZoomLevel = 14, BannerLocation = "/img/community/tankersley/tankersley-st-peters-church.jpeg" } },
             {"ruddington", new CommunityModel(){FriendlyName = "Ruddington Community Response Team", Latitude = 52.8925, Longitude = -1.150, ReferenceName = "ruddington", LinkURL = "/ruddington", ZoomLevel = 14.6, BannerLocation = "/img/community/ruddington/banner.jpg"} },
             {"ageuklsl", new CommunityModel() {FriendlyName = "Age UK Lincoln & South Lincolnshire", Latitude = 53.2304334, Longitude = -0.5435425, ReferenceName = "ageuklsl", LinkURL = "/ageuklsl", ZoomLevel = 9, DisplayOnMap = true, BannerLocation = "/img/community/ageUK/ageUKlogo.png"} },
-            {"ageukwirral", new CommunityModel() {FriendlyName = "Age UK Wirral", Latitude = 53.397320, Longitude = -3.042670, ReferenceName = "ageukwirral", LinkURL = "/ageukwirral", ZoomLevel = 9, DisplayOnMap = true, BannerLocation = "/img/community/ageUK/wirral/age-uk-wirral-banner.png"} }
+            {"ageukwirral", new CommunityModel() {FriendlyName = "Age UK Wirral", Latitude = 53.397320, Longitude = -3.042670, ReferenceName = "ageukwirral", LinkURL = "/ageukwirral", ZoomLevel = 9, DisplayOnMap = true, BannerLocation = "/img/community/ageUK/wirral/age-uk-wirral-banner.png"} },
         };
 
         public CommunityRepository(IGroupService groupService)
