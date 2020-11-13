61ec1cda1866bb686dd82458b69e6b74959e0dcf
Mike Matthews <mike@factor-50.co.uk>
2020-11-13 16:59:34 +0000

    Return requestFeedback flag with set-job-status

---
+++ HelpMyStreetFE/HelpMyStreetFE/Models/Account/Jobs/SetJobStatusResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelpMyStreetFE.Models.Account.Jobs
+{
+    public class SetJobStatusResult
+    {
+        public string NewStatus { get; set; }
+        public bool RequestFeedback { get; set; }
+    }
+}
