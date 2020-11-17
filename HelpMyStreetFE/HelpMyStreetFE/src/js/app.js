e0f54cd990fb5a2eb952d7ce097236533bb1e8c5
Mike Matthews <mike@factor-50.co.uk>
2020-11-17 12:26:08 +0000

    Keep login spinner spinning to the end

--- HelpMyStreetFE/HelpMyStreetFE/src/js/app.js
+++ HelpMyStreetFE/HelpMyStreetFE/src/js/app.js
@@ -16,32 +16,32 @@ $(function () {
     if (typeof configuration !== 'undefined') {
         firebase.init(JSON.parse(configuration.firebase));
     }
- 
+
     window.account = account;
 
     intialiseCookieConsent();
     intialiseForgottonForm(firebase, account);
 
-    $("#login-submit").click(async () => {   
-        buttonLoad($(this));   
-    try {
-        $(this).disabled = true;
-        const email = $("#email").val();
-        const password = $("#password").val();
-        const response = await account.login.login(email, password);        
-        if (!response.success) {
-        $("#login-fail-message").text(response.message);
-        $(this).disabled = false;
-      }
-    } finally {
-        buttonUnload($(this));
-        $(this).disabled = false;
+    $("#login-submit").click(async () => {
+        buttonLoad($(this));
+        try {
+            $(this).disabled = true;
+            const email = $("#email").val();
+            const password = $("#password").val();
+            const response = await account.login.login(email, password);
+            if (!response.success) {
+                $("#login-fail-message").text(response.message);
+                $(this).disabled = false;
+                buttonUnload($(this));
+            }
+        } finally {
+            $(this).disabled = false;
         }
     });
 
-    $(".yt-video-placeholder").click(function(){
+    $(".yt-video-placeholder").click(function () {
         var height = $(this).height();
         var width = $(this).width();
-        $(this).html('<iframe style="min-width: '+width+'px; height: '+height+'px" src="https://www.youtube-nocookie.com/embed/BD--FjbDKp8?rel=0&amp;cc_load_policy=1&amp;modestbranding=1;autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>')
-    })
+        $(this).html('<iframe style="min-width: ' + width + 'px; height: ' + height + 'px" src="https://www.youtube-nocookie.com/embed/BD--FjbDKp8?rel=0&amp;cc_load_policy=1&amp;modestbranding=1;autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>');
+    });
 });
