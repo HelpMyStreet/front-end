1a17055ba2f1a002cdad3dcc0e0c9f968e9a6bac
Mike Matthews <mike@factor-50.co.uk>
2020-11-24 12:04:02 +0000

    Removing partial fix of button expanding bug

--- HelpMyStreetFE/HelpMyStreetFE/src/js/shared/btn.js
+++ HelpMyStreetFE/HelpMyStreetFE/src/js/shared/btn.js
@@ -1,8 +1,7 @@
 
-export function buttonLoad(btn) {        
+export function buttonLoad(btn) { 
     btn.width(btn.width());
     btn.height(btn.height());
-    btn.css('padding', "0");
     //btn.attr('disabled', "true");
     btn.find(".text").first().hide();
     btn.find(".loader").first().show();
@@ -12,7 +11,6 @@ export function buttonLoad(btn) {
 export function buttonUnload(btn) {
     btn.width(null);
     btn.height(null);
-    btn.css('padding', "13px 40px");
     //btn.attr('disabled', "false")
     btn.find(".text").first().show();
     btn.find(".loader").first().hide();
