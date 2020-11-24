909c7d1e09a0333bbff9abcb6a4cb94edeb2f263
Mike Matthews <mike@factor-50.co.uk>
2020-11-24 12:29:54 +0000

    Removing code to handle logouts from awards code. See inactivity-monitor

--- HelpMyStreetFE/HelpMyStreetFE/src/js/shared/awards.js
+++ HelpMyStreetFE/HelpMyStreetFE/src/js/shared/awards.js
@@ -1,4 +1,4 @@
-const defaultDivContainer = "awards-component";
+﻿const defaultDivContainer = "awards-component";
 const defaultComponentEndpoint = "/account/LoadAwardsComponent";
 
 import { hmsFetch, fetchResponses } from "../shared/hmsFetch";
@@ -9,24 +9,14 @@ export async function updateAwards(componentEndpoint, divContainer) {
 
     setTimeout(async () => {
 
-    var response = await hmsFetch(componentEndpoint);
-    if (response.fetchResponse == fetchResponses.SUCCESS) {
-        var html = await response.fetchPayload;
+        var response = await hmsFetch(componentEndpoint);
+        if (response.fetchResponse == fetchResponses.SUCCESS) {
+            var html = await response.fetchPayload;
 
-        $(`.${divContainer}`).html(html);
-
-    } else if (response.fetchResponse == fetchResponses.UNAUTHORISED) {
-        if (window.location.pathname.startsWith('/account/')) {
-            // Session expired on logged-in page; redirect to login
-            window.location.replace('/account/Login?ReturnUrl=' + encodeURIComponent((window.location.pathname + window.location.search)));
-        } else {
-            // Session expired on public page; don't redirect, but also don't bother trying to get any more badge refreshes
-            clearInterval(awardsInterval);
+            $(`.${divContainer}`).html(html);
         }
-    } else {
-        //something terrible has gone wrong!
-    }
-    $("#what-is-this").click(() => { $(".tooltiptext").addClass("visible") });
-    $("#close-tooltip").click(() => { $(".tooltiptext").removeClass("visible") });
-},1000 ) //interval to allow backend to update prior to calling it, probably could be less that 1s
+
+        $("#what-is-this").click(() => { $(".tooltiptext").addClass("visible"); });
+        $("#close-tooltip").click(() => { $(".tooltiptext").removeClass("visible"); });
+    }, 1000); //interval to allow backend to update prior to calling it, probably could be less that 1s
 }
