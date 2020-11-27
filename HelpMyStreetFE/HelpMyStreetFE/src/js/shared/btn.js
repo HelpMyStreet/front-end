7b067faab6925b7b1cca49eca2b7a5076527647f
Mike Matthews <mike@factor-50.co.uk>
2020-11-27 17:01:19 +0000

    GPS-160 Removing code we don't need

--- HelpMyStreetFE/HelpMyStreetFE/src/js/shared/btn.js
+++ HelpMyStreetFE/HelpMyStreetFE/src/js/shared/btn.js
@@ -1,17 +1,10 @@
 
 export function buttonLoad(btn) { 
-    //btn.width(btn.width());
-    //btn.height(btn.height());
-    //btn.attr('disabled', "true");
     btn.find(".text").first().hide();
     btn.find(".loader").first().show();
-    
 }
 
 export function buttonUnload(btn) {
-    //btn.width(null);
-    //btn.height(null);
-    //btn.attr('disabled', "false")
     btn.find(".text").first().show();
     btn.find(".loader").first().hide();
 }
