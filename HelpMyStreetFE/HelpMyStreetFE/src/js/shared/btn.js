f820ddca6553f114085c892257720cb0e173b801
Mike Matthews <mike@factor-50.co.uk>
2020-11-27 14:40:26 +0000

    FE-517 Adding form for header login

--- HelpMyStreetFE/HelpMyStreetFE/src/js/shared/btn.js
+++ HelpMyStreetFE/HelpMyStreetFE/src/js/shared/btn.js
@@ -1,7 +1,7 @@
 
 export function buttonLoad(btn) { 
-    btn.width(btn.width());
-    btn.height(btn.height());
+    //btn.width(btn.width());
+    //btn.height(btn.height());
     //btn.attr('disabled', "true");
     btn.find(".text").first().hide();
     btn.find(".loader").first().show();
@@ -9,8 +9,8 @@ export function buttonLoad(btn) {
 }
 
 export function buttonUnload(btn) {
-    btn.width(null);
-    btn.height(null);
+    //btn.width(null);
+    //btn.height(null);
     //btn.attr('disabled', "false")
     btn.find(".text").first().show();
     btn.find(".loader").first().hide();
