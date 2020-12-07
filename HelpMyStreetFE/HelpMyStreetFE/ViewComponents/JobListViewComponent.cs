10fed84336f968573c822f59c5f8f13e84fc222f
Mike Matthews <mike@factor-50.co.uk>
2020-12-07 14:06:39 +0000

    Don't hide filters if some jobs are filtered out

--- HelpMyStreetFE/HelpMyStreetFE/ViewComponents/JobListViewComponent.cs
+++ HelpMyStreetFE/HelpMyStreetFE/ViewComponents/JobListViewComponent.cs
@@ -29,7 +29,7 @@ namespace HelpMyStreetFE.ViewComponents
             _filterService = filterService;
         }
 
-        public async Task<IViewComponentResult> InvokeAsync(JobFilterRequest jobFilterRequest, Action<int> listLengthCallback, CancellationToken cancellationToken)
+        public async Task<IViewComponentResult> InvokeAsync(JobFilterRequest jobFilterRequest, Action hideFilterPanelCallback, CancellationToken cancellationToken)
         {
             JobListViewModel jobListViewModel = new JobListViewModel();
 
@@ -84,9 +84,9 @@ namespace HelpMyStreetFE.ViewComponents
                 HighlightJob = a.JobID.Equals(jobFilterRequest.HighlightJobId),
             })));
 
-            if (listLengthCallback != null)
+            if (hideFilterPanelCallback != null && jobListViewModel.UnfilteredJobs == jobListViewModel.FilteredJobs && jobListViewModel.UnfilteredJobs <= 5)
             {
-                listLengthCallback.Invoke(jobListViewModel.UnfilteredJobs);
+                hideFilterPanelCallback.Invoke();
             }
 
             return View("JobList", jobListViewModel);
