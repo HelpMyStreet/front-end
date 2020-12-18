678195f23bb9eb562caf16937f83e8301edf8c88
Mike Matthews <mike@factor-50.co.uk>
2020-12-18 16:45:30 +0000

    Moving SupportActivities.Sentences to .PerfectTense in NuGet

--- HelpMyStreetFE/HelpMyStreetFE/Helpers/SupportActivityHelpers.cs
+++ HelpMyStreetFE/HelpMyStreetFE/Helpers/SupportActivityHelpers.cs
@@ -5,54 +5,6 @@ namespace HelpMyStreetFE.Helpers
 {
     public static class SupportActivityHelpers
     {
-        public static string Sentences(this SupportActivities activity, int count)
-        {
-            if (count == 1)
-            {
-                return activity switch
-                {
-                    SupportActivities.Shopping => "1 shopping trip",
-                    SupportActivities.FaceMask => "1 request for face coverings",
-                    SupportActivities.CheckingIn => "1 person checked on",
-                    SupportActivities.CollectingPrescriptions => "1 prescription collected",
-                    SupportActivities.Errands => "1 errand run",
-                    SupportActivities.DogWalking => "1 dog walked",
-                    SupportActivities.MealPreparation => "1 meal prepared",
-                    SupportActivities.PhoneCalls_Friendly => "1 friendly chat",
-                    SupportActivities.PhoneCalls_Anxious => "1 supportive chat",
-                    SupportActivities.HomeworkSupport => "1 homework assignment",
-                    SupportActivities.WellbeingPackage => "1 wellbeing package delivered",
-                    SupportActivities.CommunityConnector => "1 community connector task",
-                    SupportActivities.MedicalAppointmentTransport => "1 person transported to a medical appointment",
-                    SupportActivities.ColdWeatherArmy => "1 cold weather army task completed",
-                    SupportActivities.Other => "1 other task completed",
-                    _ => throw new ArgumentException(message: $"Unexpected SupportActivity: {activity}", paramName: nameof(activity))
-                };
-            }
-            else
-            {
-                return activity switch
-                {
-                    SupportActivities.Shopping => $"{count} shopping trips",
-                    SupportActivities.FaceMask => $"{count} requests for face coverings",
-                    SupportActivities.CheckingIn => $"{count} people checked on",
-                    SupportActivities.CollectingPrescriptions => $"{count} prescriptions collected",
-                    SupportActivities.Errands => $"{count} errands run",
-                    SupportActivities.DogWalking => $"{count} dogs walked",
-                    SupportActivities.MealPreparation => $"{count} meals prepared",
-                    SupportActivities.PhoneCalls_Friendly => $"{count} friendly chats",
-                    SupportActivities.PhoneCalls_Anxious => $"{count} supportive chats",
-                    SupportActivities.HomeworkSupport => $"{count} homework assignments",
-                    SupportActivities.WellbeingPackage => $"{count} wellbeing packages delivered",
-                    SupportActivities.CommunityConnector => $"{count} community connector tasks",
-                    SupportActivities.MedicalAppointmentTransport => $"{count} people transported to medical appointments",
-                    SupportActivities.ColdWeatherArmy => $"{count} cold weather army tasks completed",
-                    SupportActivities.Other => $"{count} other tasks completed",
-                    _ => throw new ArgumentException(message: $"Unexpected SupportActivity: {activity}", paramName: nameof(activity))
-                };
-            }
-        }
-
         public static string Icon(this SupportActivities activity)
         {
             return activity switch
