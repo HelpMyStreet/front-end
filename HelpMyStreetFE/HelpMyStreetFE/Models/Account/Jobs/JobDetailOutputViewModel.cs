56a37499dc54311b8dee395a6d0e83a17455e400
Mike Matthews <mike@factor-50.co.uk>
2021-01-04 15:10:56 +0000

    Removing unused class JobDetailOutputViewModel

--- HelpMyStreetFE/HelpMyStreetFE/Models/Account/Jobs/JobDetailOutputViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace HelpMyStreetFE.Models.Account.Jobs
-{
-    public class JobDetailOutputViewModel
-    {
-        public int JobID {get; set;}
-    public int UserID { get; set; }
-    }
-}
