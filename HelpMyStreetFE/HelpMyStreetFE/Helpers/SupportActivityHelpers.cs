545d008d46ec2abc82c6d1b0d82537d4dc3d75b1
Mike Matthews <mike@factor-50.co.uk>
2021-01-05 13:50:18 +0000

    GPS-330 SVG for meals on wheels icon

--- HelpMyStreetFE/HelpMyStreetFE/Helpers/SupportActivityHelpers.cs
+++ HelpMyStreetFE/HelpMyStreetFE/Helpers/SupportActivityHelpers.cs
@@ -22,7 +22,7 @@ namespace HelpMyStreetFE.Helpers
                 SupportActivities.WellbeingPackage => "package.svg",
                 SupportActivities.CommunityConnector => "phone-green.svg",
                 SupportActivities.ColdWeatherArmy => "snowflake.svg",
-                SupportActivities.MealsOnWheels => "mealsonwheels.png",
+                SupportActivities.MealsOnWheels => "mealsonwheels.svg",
                 SupportActivities.MealtimeCompanion => "mealtimecompanion.svg",
                 SupportActivities.Transport => "transport.svg",
                 _ => "question-mark.svg"
