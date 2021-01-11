76ffee462ba2837546793881e915463cedf2cbc5
Geoff Burnhill <geoff@factor-50.co.uk>
2021-01-11 16:06:31 +0000

    Create Statusbar

---
+++ HelpMyStreetFE/HelpMyStreetFE/Models/Account/Jobs/StatusBarViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace HelpMyStreetFE.Models.Account.Jobs
+{
+    public class StatusBarViewModel
+    {
+        public int TotalJobs { get; set; }
+        public int AcceptedJobs { get; set; }
+    }
+}
