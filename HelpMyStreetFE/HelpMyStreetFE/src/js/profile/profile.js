bbbde7849a9d47b980e8dc800680d4e63386ea4e
Geoff Burnhill <geoff@factor-50.co.uk>
2021-01-15 17:52:57 +0000

    More shiny features
     - Animation, JS Update ability

--- HelpMyStreetFE/HelpMyStreetFE/src/js/profile/profile.js
+++ HelpMyStreetFE/HelpMyStreetFE/src/js/profile/profile.js
@@ -5,6 +5,19 @@ import { initialiseVolunteerList } from "./volunteers";
 import { hmsFetch, fetchResponses } from "../shared/hmsFetch";
 import { updateAwards } from "../shared/awards";
 
+function updateJobBar() {
+    var jbwidth = $(".status-circle").width();
+    var dashoffset = aJobs / totalJobs * 3.141 * 0.7 * jbwidth;
+    $("#jobBar").animate({ 'stroke-dashoffset': dashoffset });
+    $(".status-circle span").html(`${aJobs}/${totalJobs}`);
+    if (aJobs == totalJobs) {
+        $('#jobBar').animate({"fill-opacity": 1 });
+        $('#tick').show();
+    } else {
+        $('#tick').hide();
+    }
+}
+
 $(document).ready(function () {
   const isVerified = (initObj && initObj.isVerified == "True");
   initialiseVerification(isVerified);
@@ -12,4 +25,12 @@ $(document).ready(function () {
   initialiseGenericExpanders();
   initialiseVolunteerList();
   updateAwards();
+  updateJobBar();
+
+  $('#addJob').click(function () {
+    if (aJobs != totalJobs) {
+        aJobs = aJobs + 1;
+    }
+    updateJobBar();
+  });
 });
