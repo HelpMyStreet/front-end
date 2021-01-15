ce61d422d0d34a3ab27ed6c52e33aad0ab9ff5bf
Geoff Burnhill <geoff@factor-50.co.uk>
2021-01-15 18:12:57 +0000

    Update bar shaped bar

--- HelpMyStreetFE/HelpMyStreetFE/src/js/profile/profile.js
+++ HelpMyStreetFE/HelpMyStreetFE/src/js/profile/profile.js
@@ -6,15 +6,22 @@ import { hmsFetch, fetchResponses } from "../shared/hmsFetch";
 import { updateAwards } from "../shared/awards";
 
 function updateJobBar() {
-    var jbwidth = $(".status-circle").width();
-    var dashoffset = aJobs / totalJobs * 3.141 * 0.7 * jbwidth;
-    $("#jobBar").animate({ 'stroke-dashoffset': dashoffset });
-    $(".status-circle span").html(`${aJobs}/${totalJobs}`);
-    if (aJobs == totalJobs) {
-        $('#jobBar').animate({"fill-opacity": 1 });
-        $('#tick').show();
-    } else {
-        $('#tick').hide();
+
+    if ($('.status-circle')[0]) {
+        var jbwidth = $(".status-circle").width();
+        var dashoffset = aJobs / totalJobs * 3.141 * 0.7 * jbwidth;
+        $("#jobBar").animate({ 'stroke-dashoffset': dashoffset });
+        $(".status-circle span").html(`${aJobs}/${totalJobs}`);
+        if (aJobs == totalJobs) {
+            $('#jobBar').animate({ "fill-opacity": 1 });
+            $('#tick').show();
+        } else {
+            $('#tick').hide();
+        }
+    }
+    else {
+        $(".status-bar span").html(`${aJobs}/${totalJobs}`);
+        $("#jobBar").animate({ "height": `${(aJobs / totalJobs * 100)}%`, "y": `${((totalJobs - aJobs) / totalJobs * 100)}%` });
     }
 }
 
