5dc911bde5cde2498a04f8507f3b3e83a963799f
Mike Matthews <mike@factor-50.co.uk>
2021-01-18 15:47:47 +0000

    Group shifts

---
+++ HelpMyStreetFE/HelpMyStreetFE/Helpers/ShiftRequestExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HelpMyStreet.Utils.Enums;
+using HelpMyStreet.Utils.Models;
+
+namespace HelpMyStreetFE.Helpers
+{
+    public static class ShiftRequestExtensions
+    {
+        public static JobStatuses JobStatus(this ShiftRequest shiftRequest)
+        {
+            return JobStatuses.InProgress;
+        }
+    }
+}
