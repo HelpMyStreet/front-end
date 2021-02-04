3d5a915d221a5fae6a195e4293a2185d6d42481a
Mike Matthews <mike@factor-50.co.uk>
2021-02-04 10:17:07 +0000

    Making progress bar more flexible

--- HelpMyStreetFE/HelpMyStreetFE/Models/RequestHelp/Stages/Detail/RequestHelpDetailStageViewModel.cs
+++ HelpMyStreetFE/HelpMyStreetFE/Models/RequestHelp/Stages/Detail/RequestHelpDetailStageViewModel.cs
@@ -13,7 +13,7 @@ namespace HelpMyStreetFE.Models.RequestHelp.Stages.Detail
         public string Organisation { get; set; }
         public RequestorDetails Requestor { get; set; }
         public RecipientDetails Recipient { get; set; }
-        public string FriendlyName { get; set; } = "Detail";
+        public string FriendlyName { get; set; } = "Details";
         public string TemplateName { get; set; } = "RequestHelpDetailStageViewModel";
         public RequestorType Type { get; set; }
         public QuestionsViewModel Questions { get; set; }
