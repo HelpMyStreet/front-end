6d5e2439997674b7405f37db5049ec4024620de7
Mike Matthews <mike@factor-50.co.uk>
2021-03-03 10:47:39 +0000

    Introducing constant DatePickerHelpers.TIME_FIELD_FORMAT

--- HelpMyStreetFE/HelpMyStreetFE/Helpers/DatePickerHelpers.cs
+++ HelpMyStreetFE/HelpMyStreetFE/Helpers/DatePickerHelpers.cs
@@ -8,5 +8,6 @@ namespace HelpMyStreetFE.Helpers
     public class DatePickerHelpers
     {
         public const string DATE_PICKER_DATE_FORMAT = "dd / MM / yyyy";
+        public const string TIME_FIELD_FORMAT = "HH:mm";
     }
 }
