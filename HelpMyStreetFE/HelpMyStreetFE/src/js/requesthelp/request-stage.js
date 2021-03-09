d655239da6b8c8478917fe399291927ac95244f9
Mike Matthews <mike@factor-50.co.uk>
2021-03-09 15:50:18 +0000

    Removing magic numbers from Timeframe tiles

--- HelpMyStreetFE/HelpMyStreetFE/src/js/requesthelp/request-stage.js
+++ HelpMyStreetFE/HelpMyStreetFE/src/js/requesthelp/request-stage.js
@@ -1,4 +1,4 @@
-import { validateFormData, scrollToFirstError } from "../shared/validator";
+﻿import { validateFormData, scrollToFirstError } from "../shared/validator";
 import { buttonLoad, buttonUnload } from "../shared/btn";
 import { trackEvent } from "../shared/tracking-helper";
 import { loadQuestions, validateQuestions } from "./requesthelp-shared.js";
@@ -10,8 +10,10 @@ export function intialiseRequestStage() {
 
     trackEvent("Request form", "View 0.request", "", 0);
 
-    const taskId = $('input[name="currentStep.SelectedTask.Id"]').val();
-    updateOptionsForActivity(taskId);
+    $('*[data-type="activities"].selected').each(function () {
+        updateOptionsForActivity($(this).data("support-activity"));
+    });
+
     if ($('#datepicker').length > 0) {
         datepickerLoad('datepicker', 'dateselectionError', dateValidationSchemes.FUTURE_DATES_6M);
     }
@@ -81,83 +83,74 @@ var handleTimeFrame = function (el) {
 }
 
 var handleActivity = function (el) {
-  $('*[data-type="activities"]').removeClass("selected");
-  el.addClass("selected");
-  let taskId = el.attr("data-id");
-  $('input[name="currentStep.SelectedTask.Id"]').val(taskId);
+    $('*[data-type="activities"]').removeClass("selected");
+    el.addClass("selected");
+    let taskId = el.attr("data-id");
+    $('input[name="currentStep.SelectedTask.Id"]').val(taskId);
 
-  let selectedValue = $(el).find('.tiles__tile__content__header').first().html();
-  trackEvent("Request form", "Select activity", selectedValue, 0);
+    let selectedValue = $(el).find('.tiles__tile__content__header').first().html();
+    trackEvent("Request form", "Select activity", selectedValue, 0);
 
-  let scrollTop = $(window).scrollTop();
+    let scrollTop = $(window).scrollTop();
 
-  updateOptionsForActivity(taskId);
-  loadQuestions(taskId, function () {
-    if (scrollTop == $(window).scrollTop()) {
-      $('html, body').animate({
-        scrollTop:
-          $(el).parentsUntil("form").last().next().offset().top
-      }, 1000);
-    }
-  });
+    updateOptionsForActivity(el.data("support-activity"));
+    loadQuestions(taskId, function () {
+        if (scrollTop == $(window).scrollTop()) {
+            $('html, body').animate({
+                scrollTop:
+                    $(el).parentsUntil("form").last().next().offset().top
+            }, 1000);
+        }
+    });
 }
 
-var updateOptionsForActivity = function (taskId) {
+var updateOptionsForActivity = function (supportActivity) {
     let requestHelpFormVariant = JSON.parse($('input[name="requestHelp"]').val()).RequestHelpFormVariant;
-    if (taskId == 12) { // facemask   
+    if (supportActivity == 'FaceMask') {
         $('#requestorFor_3').parent().show(); // onbehalf of organisation
+
+        $('*[data-type="timeframe"]').each(function () { $(this).parent().hide(); });
+
+        $('*[data-type="timeframe"][data-duedatetype="Before"]').each(function () {
+            const days = parseInt($(this).data('val'));
+            if (days < 7) { $(this).parent().hide(); }
+            else { $(this).parent().show(); }
+        });
+
         if ($('#requestorFor_3').hasClass("selected")) {
             $('input[name="currentStep.SelectedRequestor.Id"]').val($('#requestorFor_3').attr("data-id"));
         }
-        displayTodayHelpNeededOptions(false);
+    } else if (supportActivity == 'VolunteerSupport' || supportActivity == 'VaccineSupport') {
+        $('*[data-type="timeframe"]').each(function () { $(this).parent().hide(); });
+        $('*[data-type="timeframe"][data-duedatetype="SpecificStartAndEndTimes"]').each(function () { $(this).parent().show(); });
     } else {
-        if (requestHelpFormVariant == 11 || requestHelpFormVariant == 14 || requestHelpFormVariant == 16) { // Age UK Kent Admin Pages
-            $('#requestorFor_1').parent().hide(); // myself
-            $('#requestorFor_2').parent().show(); // someone else
-            $('#requestorFor_3').parent().hide(); // onbehalf of organisation
-            displayTodayHelpNeededOptions(true);
-        } else if (requestHelpFormVariant == 19) { // Sandbox
-            $('#requestorFor_3').parent().show(); // onbehalf of organisation
-            if (taskId == 22 || taskId == 24) { // Volunteer Support, Vaccine Support
-                $('#requestorFor_1').parent().hide(); // myself
-                $('#requestorFor_2').parent().hide(); // someone else
-                $('#time_1').parent().hide(); // Today
-                $('#time_2').parent().hide(); // 24H
-                $('#time_3').parent().hide(); // Week
-                $('#time_4').parent().hide(); // When convenient
-                $('#time_5').parent().hide(); // Other
-                $('#time_7').parent().show(); // Date and Time
-                $('#start_time_7').parent().show(); // Date and Time
-                $('#end_time_7').parent().show(); // Date and Time
-            } else {
-                $('#requestorFor_1').parent().show(); // myself
-                $('#requestorFor_2').parent().show(); // someone else
-                $('#time_1').parent().show(); // Today
-                $('#time_2').parent().show(); // 24H
-                $('#time_3').parent().show(); // Week
-                $('#time_4').parent().show(); // When convenient
-                $('#time_5').parent().show(); // Other
-                $('#time_7').parent().hide(); // Date and Time
-                $('#start_time_7').parent().hide(); // Date and Time
-                $('#end_time_7').parent().hide(); // Date and Time
-            }
-            $('div[data-type="request-for"]').removeClass("selected");
-            $('div[data-type="timeframe"]').removeClass("selected");
-            $('input[name="currentStep.SelectedRequestor.Id"]').val("");
-            $('input[name="currentStep.selectedTimeFrame.Id"]').val("");
-        } else {
-            $('#requestorFor_3').parent().hide();
-            var currentRequestedFor = $('input[name="currentStep.SelectedRequestor.Id"]').val();
-            // if they have previously selected On behalf of organisation,
-            //set selected RequestorID as empty since that option is only available to facemasks
-            if (currentRequestedFor == $('#requestorFor_3').attr("data-id")) {
-                $('input[name="currentStep.SelectedRequestor.Id"]').val("");
-            }
-            displayTodayHelpNeededOptions(true);
-        }
+        $('*[data-type="timeframe"]').each(function () { $(this).parent().show(); });
+        $('*[data-type="timeframe"][data-duedatetype="SpecificStartAndEndTimes"]').each(function () { $(this).parent().hide(); });
     }
 
+    if ($('*[data-type="timeframe"].selected:not(:visible)').length > 0) {
+        $('*[data-type="timeframe"]').removeClass("selected");
+        $('input[name="currentStep.SelectedTimeFrame.Id"]').val("");
+    }
 
+    if (requestHelpFormVariant == 11 || requestHelpFormVariant == 14 || requestHelpFormVariant == 16) { // Age UK Kent Admin Pages
+        $('#requestorFor_1').parent().hide(); // myself
+        $('#requestorFor_2').parent().show(); // someone else
+        $('#requestorFor_3').parent().hide(); // onbehalf of organisation
+        displayTodayHelpNeededOptions(true);
+    } else if (requestHelpFormVariant == 19) { // Sandbox
+        $('#requestorFor_3').parent().show(); // onbehalf of organisation
+        $('div[data-type="request-for"]').removeClass("selected");
+        $('input[name="currentStep.SelectedRequestor.Id"]').val("");
+    } else {
+        $('#requestorFor_3').parent().hide();
+        var currentRequestedFor = $('input[name="currentStep.SelectedRequestor.Id"]').val();
+        // if they have previously selected On behalf of organisation,
+        //set selected RequestorID as empty since that option is only available to facemasks
+        if (currentRequestedFor == $('#requestorFor_3').attr("data-id")) {
+            $('input[name="currentStep.SelectedRequestor.Id"]').val("");
+        }
+    }
 
     // preselect value if theres only one
     if ($('.requestorFor:visible').length == 1) {
@@ -165,16 +158,6 @@ var updateOptionsForActivity = function (taskId) {
         let selectedId = $('.requestorFor:visible').attr("data-id");
         $('input[name="currentStep.SelectedRequestor.Id"]').val(selectedId);
     }
-}
-
-function displayTodayHelpNeededOptions(show) {
-  if (!show) {
-    $('#time_1').parent().hide();
-    $('#time_2').parent().hide();
-  } else {
-    $('#time_1').parent().show();
-    $('#time_2').parent().show();
-  }
 }
 
 function validateDateIfNecessary() {
