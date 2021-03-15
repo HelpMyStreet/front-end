769a7fa00e48729bc212a8b9ba30894790826db0
Mike Matthews <mike@factor-50.co.uk>
2021-03-15 17:16:42 +0000

    FE-620 Introducing TileSelectorViewModel

---
+++ HelpMyStreetFE/HelpMyStreetFE/Models/IBasicTileViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelpMyStreetFE.Models
+{
+    public interface IBasicTileViewModel
+    {
+        public string ID { get; }
+        public string Value { get; }
+        public string Description { get; }
+        public bool IsSelected { get; set; }
+        public string DataType { get; }
+    }
+}
