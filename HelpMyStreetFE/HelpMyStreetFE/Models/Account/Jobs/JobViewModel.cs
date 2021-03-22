3a12e3424a1f1aa20f1c770ed246732242b43a76
Geoff Burnhill <geoff@factor-50.co.uk>
2021-03-22 12:51:57 +0000

    FE-773

--- HelpMyStreetFE/HelpMyStreetFE/Models/Account/Jobs/JobViewModel.cs
+++ HelpMyStreetFE/HelpMyStreetFE/Models/Account/Jobs/JobViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using HelpMyStreet.Utils.Models;
 using HelpMyStreet.Utils.Enums;
+using HelpMyStreet.Utils.Extensions;
+using System.Collections.Generic;
+using System;
 
 namespace HelpMyStreetFE.Models.Account.Jobs
 {
@@ -11,5 +14,56 @@ namespace HelpMyStreetFE.Models.Account.Jobs
         public bool HighlightJob { get; set; }
         public LocationWithDistance Location { get; set; }
         public int? JobListGroupId { get; set; }
+        public string ListLocalityDescription { get => GetLocalityInformation(); }
+
+        private string GetLocalityInformation()
+        {
+            JobStatuses jstatus = new JobStatuses();
+            string postCode = "";
+            string distance = "";
+
+            var personalDetailsComponents = new List<PersonalDetailsComponent>();
+
+            switch (Item)
+            {
+                case JobSummary js:
+                    jstatus = js.JobStatus;
+                    postCode = js.PostCode;
+                    distance = $"{Math.Round(js.DistanceInMiles, 1)}";
+                    personalDetailsComponents = js.SupportActivity.PersonalDetailsComponent(RequestRoles.Recipient);
+                    break;
+                case ShiftJob sj:
+                    jstatus = sj.JobStatus;
+                    postCode = Location.LocationDetails.Name;
+                    distance = $"{Math.Round(Location.Distance, 1)}";
+                    break;
+                case RequestSummary rs:
+                    postCode = rs.PostCode;
+                    distance = "";
+                    break;
+            }
+
+            if (UserRole == RequestRoles.GroupAdmin)
+            {
+                return $"{postCode}";
+            }
+            else if (jstatus == JobStatuses.Open || jstatus == JobStatuses.New)
+            {
+                return $"{postCode.Split(" ")[0]}, {distance} miles away";
+            }
+            else if (jstatus == JobStatuses.Accepted || jstatus == JobStatuses.InProgress)
+            {
+                if (personalDetailsComponents.Contains(PersonalDetailsComponent.Postcode))
+                {
+                    return $"{postCode}";
+                }
+                else {
+                    return $"{postCode.Split(" ")[0]}";
+                }
+            }
+
+            return "";
+        }
     }
+    
 }
