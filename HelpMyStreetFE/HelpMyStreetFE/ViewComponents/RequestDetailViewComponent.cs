a4dd26215c637c381f4424b52a1a9ad4fffa8540
Mike Matthews <mike@factor-50.co.uk>
2021-07-15 17:29:34 +0100

    Tidying

--- HelpMyStreetFE/HelpMyStreetFE/ViewComponents/RequestDetailViewComponent.cs
+++ HelpMyStreetFE/HelpMyStreetFE/ViewComponents/RequestDetailViewComponent.cs
@@ -12,7 +12,6 @@ using System.Collections.Generic;
 using System.Linq;
 using HelpMyStreet.Utils.Enums;
 using HelpMyStreetFE.Services;
-using HelpMyStreetFE.Models.Account;
 
 namespace HelpMyStreetFE.ViewComponents
 {
