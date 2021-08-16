7ac40e973ab4b43b5ca2c3099aada6b332eb41c7
Mike Matthews <mike@factor-50.co.uk>
2021-08-16 17:12:25 +0100

    Introducing _Question partial

---
+++ HelpMyStreetFE/HelpMyStreetFE/Models/Account/Jobs/QuestionViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HelpMyStreet.Utils.Models;
+
+namespace HelpMyStreetFE.Models.Account.Jobs
+{
+    public class QuestionViewModel
+    {
+        public QuestionViewModel(Question question, bool isEditable = false)
+        {
+            Question = question;
+            IsEditable = isEditable;
+        }
+
+        public Question Question { get; set; }
+        public bool IsEditable { get; set; }
+    }
+}
