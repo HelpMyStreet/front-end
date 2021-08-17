8c37b576f14b21d1fecd73753bc470558c924e8d
Mike Matthews <mike@factor-50.co.uk>
2021-08-17 10:07:48 +0100

    Adding IDs to _Question partial

--- HelpMyStreetFE/HelpMyStreetFE/Models/Account/Jobs/QuestionViewModel.cs
+++ HelpMyStreetFE/HelpMyStreetFE/Models/Account/Jobs/QuestionViewModel.cs
@@ -8,12 +8,14 @@ namespace HelpMyStreetFE.Models.Account.Jobs
 {
     public class QuestionViewModel
     {
-        public QuestionViewModel(Question question, bool isEditable = false)
+        public QuestionViewModel(JobBasic job, Question question, bool isEditable = false)
         {
+            Job = job;
             Question = question;
             IsEditable = isEditable;
         }
 
+        public JobBasic Job { get; set; }
         public Question Question { get; set; }
         public bool IsEditable { get; set; }
     }
