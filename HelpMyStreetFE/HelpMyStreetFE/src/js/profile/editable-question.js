f669d5b7fb9ef86199b39e7c6833582911f5ddc7
Mike Matthews <mike@factor-50.co.uk>
2021-08-17 14:32:47 +0100

    A little js magic

---
+++ HelpMyStreetFE/HelpMyStreetFE/src/js/profile/editable-question.js
@@ -0,0 +1,26 @@
+﻿
+
+export async function initialiseEditableQuestions() {
+    $('body').on('click', '.editable-question .edit', function (e) {
+        e.preventDefault();
+        const questionContainer = $(this).closest('.editable-question');
+        questionContainer.find('div.answer').hide();
+        questionContainer.find('form').show();
+    });
+
+    $('body').on('click', '.editable-question .save-changes', function (e) {
+        e.preventDefault();
+        const questionContainer = $(this).closest('.editable-question');
+        questionContainer.find('div.answer').show();
+        questionContainer.find('form').hide();
+    });
+
+    $('body').on('click', '.editable-question .cancel-editing', function (e) {
+        e.preventDefault();
+        const questionContainer = $(this).closest('.editable-question');
+        questionContainer.find('div.answer').show();
+        questionContainer.find('form').hide();
+    });
+
+
+};
