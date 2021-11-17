e64d0cdfda7fa93a3c2421ae7aa275ece832a248
Mike Matthews <mike@factor-50.co.uk>
2021-11-17 11:58:57 +0000

    Fixing tests

--- HelpMyStreetFE/HelpMyStreetFE.UnitTests/Services/RequestCachingServiceTests.cs
+++ HelpMyStreetFE/HelpMyStreetFE.UnitTests/Services/RequestCachingServiceTests.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using HelpMyStreet.Cache;
+using HelpMyStreet.Cache.Models;
 using HelpMyStreet.Utils.Models;
 using HelpMyStreetFE.Repositories;
 using HelpMyStreetFE.Services.Requests;
@@ -22,7 +23,7 @@ namespace HelpMyStreetFE.UnitTests.Services
 
         private RequestCachingService _classUnderTest;
 
-        private Dictionary<string, RequestSummary> _requestSummaries;
+        private Dictionary<string, CachedItemWrapper<RequestSummary>> _requestSummaries;
         private CancellationToken _cancellationToken;
 
         private int _waitForBackgroundThreadToCompleteMs = 100;
@@ -34,27 +35,29 @@ namespace HelpMyStreetFE.UnitTests.Services
             _cache = new Mock<IMemDistCache<RequestSummary>>();
             _logger = new Mock<ILogger<RequestCachingService>>();
 
-            _requestSummaries = new Dictionary<string, RequestSummary> {
-                { "request-caching-service-request-1", new RequestSummary{ RequestID = 1} },
-                { "request-caching-service-request-2", new RequestSummary{ RequestID = 2} },
-                { "request-caching-service-request-3", new RequestSummary{ RequestID = 3} },
+            _requestSummaries = new Dictionary<string, CachedItemWrapper<RequestSummary>> {
+                { "request-caching-service-request-1", new CachedItemWrapper<RequestSummary>(new RequestSummary{ RequestID = 1}, DateTime.MaxValue) },
+                { "request-caching-service-request-2", new CachedItemWrapper<RequestSummary>(new RequestSummary{ RequestID = 2}, DateTime.MaxValue) },
+                { "request-caching-service-request-3", new CachedItemWrapper<RequestSummary>(new RequestSummary{ RequestID = 3}, DateTime.MaxValue) },
+                { "request-caching-service-stale-request-98", new CachedItemWrapper<RequestSummary>(new RequestSummary{ RequestID = 98}, DateTime.MinValue) },
+                { "request-caching-service-stale-request-99", new CachedItemWrapper<RequestSummary>(new RequestSummary{ RequestID = 99}, DateTime.MinValue) },
             };
 
             _cancellationToken = new CancellationToken();
 
-            _cache.Setup(x => x.GetCachedDataAsync(It.IsAny<Func<CancellationToken, Task<RequestSummary>>>(),
+            _cache.Setup(x => x.GetCachedDataInWrapperAsync(It.IsAny<Func<CancellationToken, Task<RequestSummary>>>(),
                                                    It.IsAny<string>(),
                                                    It.IsAny<RefreshBehaviour>(),
                                                    It.IsAny<CancellationToken>(),
-                                                   It.IsAny<NotInCacheBehaviour>(), 
+                                                   It.IsAny<NotInCacheBehaviour>(),
                                                    null))
                                     .ReturnsAsync((Func<CancellationToken, Task<RequestSummary>> dataGetter,
                                                    string key,
                                                    RefreshBehaviour refreshBehaviour,
                                                    CancellationToken cancellationToken,
-                                                   NotInCacheBehaviour notInCacheBehaviour, 
+                                                   NotInCacheBehaviour notInCacheBehaviour,
                                                    Func<DateTimeOffset, DateTimeOffset> whenDataIsStaleDelegate)
-                                                        => _requestSummaries.ContainsKey(key) ? _requestSummaries[key] : null);
+                                                        => _requestSummaries.ContainsKey(key) ? _requestSummaries[key] : new CachedItemWrapper<RequestSummary>(null, DateTime.MinValue));
 
             _repo.Setup(x => x.GetRequestSummariesAsync(It.IsAny<IEnumerable<int>>()))
                                     .ReturnsAsync((IEnumerable<int> requestIDs) => requestIDs.Select(r => new RequestSummary { RequestID = r }));
@@ -69,7 +72,7 @@ namespace HelpMyStreetFE.UnitTests.Services
 
             var result = await _classUnderTest.GetRequestSummariesAsync(ids, false, _cancellationToken);
 
-            _cache.Verify(x => x.GetCachedDataAsync(It.IsAny<Func<CancellationToken, Task<RequestSummary>>>(), It.IsAny<string>(), RefreshBehaviour.DontRefreshData, _cancellationToken, NotInCacheBehaviour.DontGetData, null), Times.Exactly(3));
+            _cache.Verify(x => x.GetCachedDataInWrapperAsync(It.IsAny<Func<CancellationToken, Task<RequestSummary>>>(), It.IsAny<string>(), RefreshBehaviour.DontRefreshData, _cancellationToken, NotInCacheBehaviour.DontGetData, null), Times.Exactly(3));
             _repo.Verify(x => x.GetRequestSummariesAsync(It.IsAny<IEnumerable<int>>()), Times.Never);
             _cache.Verify(x => x.RefreshDataAsync(It.IsAny<Func<CancellationToken, Task<RequestSummary>>>(), It.IsAny<string>(), It.IsAny<CancellationToken>(), It.IsAny<Func<DateTimeOffset, DateTimeOffset>>()), Times.Never);
             Assert.AreEqual(3, result.Count());
@@ -82,10 +85,23 @@ namespace HelpMyStreetFE.UnitTests.Services
 
             var result = await _classUnderTest.GetRequestSummariesAsync(ids, true, _cancellationToken);
 
-            _cache.Verify(x => x.GetCachedDataAsync(It.IsAny<Func<CancellationToken, Task<RequestSummary>>>(), It.IsAny<string>(), RefreshBehaviour.DontRefreshData, _cancellationToken, NotInCacheBehaviour.DontGetData, null), Times.Exactly(3));
+            _cache.Verify(x => x.GetCachedDataInWrapperAsync(It.IsAny<Func<CancellationToken, Task<RequestSummary>>>(), It.IsAny<string>(), RefreshBehaviour.DontRefreshData, _cancellationToken, NotInCacheBehaviour.DontGetData, null), Times.Exactly(3));
             _repo.Verify(x => x.GetRequestSummariesAsync(It.IsAny<IEnumerable<int>>()), Times.Never);
             _cache.Verify(x => x.RefreshDataAsync(It.IsAny<Func<CancellationToken, Task<RequestSummary>>>(), It.IsAny<string>(), It.IsAny<CancellationToken>(), It.IsAny<Func<DateTimeOffset, DateTimeOffset>>()), Times.Never);
             Assert.AreEqual(3, result.Count());
+        }
+
+        [Test]
+        public async Task GetRequestSummariesAsync_WhenStale_WaitForData_GetsFromCacheAndTriggersBackgroundRefresh()
+        {
+            var ids = new List<int> { 1, 1, 2, 3, 98, 99 };
+
+            var result = await _classUnderTest.GetRequestSummariesAsync(ids, true, _cancellationToken);
+
+            _cache.Verify(x => x.GetCachedDataInWrapperAsync(It.IsAny<Func<CancellationToken, Task<RequestSummary>>>(), It.IsAny<string>(), RefreshBehaviour.DontRefreshData, _cancellationToken, NotInCacheBehaviour.DontGetData, null), Times.Exactly(5));
+            _repo.Verify(x => x.GetRequestSummariesAsync(It.IsAny<IEnumerable<int>>()), Times.Once);
+            _cache.Verify(x => x.RefreshDataAsync(It.IsAny<Func<CancellationToken, Task<RequestSummary>>>(), It.IsAny<string>(), It.IsAny<CancellationToken>(), It.IsAny<Func<DateTimeOffset, DateTimeOffset>>()), Times.Exactly(2));
+            Assert.AreEqual(5, result.Count());
         }
 
         [Test]
@@ -97,7 +113,7 @@ namespace HelpMyStreetFE.UnitTests.Services
 
             await Task.Delay(_waitForBackgroundThreadToCompleteMs); // wait for background thread
 
-            _cache.Verify(x => x.GetCachedDataAsync(It.IsAny<Func<CancellationToken, Task<RequestSummary>>>(), It.IsAny<string>(), RefreshBehaviour.DontRefreshData, _cancellationToken, NotInCacheBehaviour.DontGetData, null), Times.Exactly(6));
+            _cache.Verify(x => x.GetCachedDataInWrapperAsync(It.IsAny<Func<CancellationToken, Task<RequestSummary>>>(), It.IsAny<string>(), RefreshBehaviour.DontRefreshData, _cancellationToken, NotInCacheBehaviour.DontGetData, null), Times.Exactly(6));
             _repo.Verify(x => x.GetRequestSummariesAsync(It.IsAny<IEnumerable<int>>()), Times.Once);
             _cache.Verify(x => x.RefreshDataAsync(It.IsAny<Func<CancellationToken, Task<RequestSummary>>>(), It.IsAny<string>(), It.IsAny<CancellationToken>(), It.IsAny<Func<DateTimeOffset, DateTimeOffset>>()), Times.Exactly(3));
             Assert.AreEqual(0, result.Count());
@@ -110,7 +126,7 @@ namespace HelpMyStreetFE.UnitTests.Services
 
             var result = await _classUnderTest.GetRequestSummariesAsync(ids, true, _cancellationToken);
 
-            _cache.Verify(x => x.GetCachedDataAsync(It.IsAny<Func<CancellationToken, Task<RequestSummary>>>(), It.IsAny<string>(), RefreshBehaviour.DontRefreshData, _cancellationToken, NotInCacheBehaviour.DontGetData, null), Times.Exactly(6));
+            _cache.Verify(x => x.GetCachedDataInWrapperAsync(It.IsAny<Func<CancellationToken, Task<RequestSummary>>>(), It.IsAny<string>(), RefreshBehaviour.DontRefreshData, _cancellationToken, NotInCacheBehaviour.DontGetData, null), Times.Exactly(6));
             _repo.Verify(x => x.GetRequestSummariesAsync(It.IsAny<IEnumerable<int>>()), Times.Once);
             _cache.Verify(x => x.RefreshDataAsync(It.IsAny<Func<CancellationToken, Task<RequestSummary>>>(), It.IsAny<string>(), It.IsAny<CancellationToken>(), It.IsAny<Func<DateTimeOffset, DateTimeOffset>>()), Times.Exactly(3));
             Assert.AreEqual(6, result.Count());
@@ -121,7 +137,7 @@ namespace HelpMyStreetFE.UnitTests.Services
         {
             var result = await _classUnderTest.GetRequestSummaryAsync(2, _cancellationToken);
 
-            _cache.Verify(x => x.GetCachedDataAsync(It.IsAny<Func<CancellationToken, Task<RequestSummary>>>(), "request-caching-service-request-2", RefreshBehaviour.DontWaitForFreshData, _cancellationToken, NotInCacheBehaviour.WaitForData, null), Times.Once);
+            _cache.Verify(x => x.GetCachedDataInWrapperAsync(It.IsAny<Func<CancellationToken, Task<RequestSummary>>>(), "request-caching-service-request-2", RefreshBehaviour.DontWaitForFreshData, _cancellationToken, NotInCacheBehaviour.WaitForData, null), Times.Once);
             Assert.AreEqual(2, result.RequestID);
         }
 
