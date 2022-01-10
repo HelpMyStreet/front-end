2b8d1a2d5456fc05edc508206a30b20e5e14c7c5
Mike Matthews <mike@factor-50.co.uk>
2022-01-10 10:57:32 +0000

    FE-1305 add explicit error

--- HelpMyStreetFE/HelpMyStreetFE/Services/Requests/RequestLocationService.cs
+++ HelpMyStreetFE/HelpMyStreetFE/Services/Requests/RequestLocationService.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -29,6 +29,11 @@ namespace HelpMyStreetFE.Services.Requests
         public async Task<JobLocation> LocateJob(int jobId, int userId, CancellationToken cancellationToken)
         {
             var job = await _jobCachingService.GetJobBasicAsync(jobId, cancellationToken);
+
+            if (job == null)
+            {
+                throw new Exception($"Failed to locate job {jobId} for user {userId}");
+            }
 
             if (job.VolunteerUserID == userId && job.JobStatus != JobStatuses.Open)
             {
