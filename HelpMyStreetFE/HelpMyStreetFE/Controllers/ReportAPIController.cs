aaf6c3228e3fb3e89f1e6383d2ee700e687968c4
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2022-02-01 13:44:20 +0000

    Add security to ReportAPIController Remove unused classes

--- HelpMyStreetFE/HelpMyStreetFE/Controllers/ReportAPIController.cs
+++ HelpMyStreetFE/HelpMyStreetFE/Controllers/ReportAPIController.cs
@@ -1,21 +1,14 @@
-﻿using System.Linq;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
-
-using System.Collections.Generic;
-using HelpMyStreet.Contracts.AddressService.Request;
-using HelpMyStreet.Contracts.AddressService.Response;
-using HelpMyStreet.Contracts.Shared;
 using HelpMyStreetFE.Models.Reponses;
-using HelpMyStreetFE.Models.Community;
-using HelpMyStreetFE.Services;
 using HelpMyStreetFE.Repositories;
-using HelpMyStreetFE.Services.Users;
 using HelpMyStreet.Utils.Enums;
 using System.Threading;
 using HelpMyStreet.Contracts.ReportService;
 using Newtonsoft.Json;
 using System;
+using HelpMyStreetFE.Services.Users;
+using HelpMyStreetFE.Services.Groups;
 
 namespace HelpMyStreetFE.Controllers
 {
@@ -24,15 +17,30 @@ namespace HelpMyStreetFE.Controllers
     public class ReportAPIController : ControllerBase
     {
         private readonly IReportRepository _reportRepository;
-        public ReportAPIController(IReportRepository reportRepository)
+        private readonly IAuthService _authService;
+        private readonly IGroupMemberService _groupMemberService;
+        public ReportAPIController(IReportRepository reportRepository, IAuthService authService, IGroupMemberService groupMemberService)
         {
             _reportRepository = reportRepository;
+            _authService = authService;
+            _groupMemberService = groupMemberService;
         }
 
         [HttpGet("getReport")]
         public async Task<ActionResult<GetReportResponse>> GetReport(Charts chart,int groupId, ChartTypes chartType, DateTime dateFrom, DateTime dateTo, CancellationToken cancellationToken)
         {
-            //TODO check user is authorised for this particular group
+            var user = await _authService.GetCurrentUser(cancellationToken);
+
+            if (user == null)
+            {
+                throw new UnauthorizedAccessException("No user in session");
+            }
+
+            if (!await _groupMemberService.GetUserHasRole(user.ID, groupId, GroupRoles.ShowCharts, true, cancellationToken))
+            {
+                throw new UnauthorizedAccessException("user does not have permission to view charts");
+            }
+
             Chart chartModel = await _reportRepository.GetChart(chart, groupId, dateFrom, dateTo, cancellationToken);        
             GetReportResponse getReportResponse = new GetReportResponse(chartModel, chartType);
 
