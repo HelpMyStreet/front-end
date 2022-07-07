915e1494379564be4c9392c25dc5d64177e738b6
Mike Matthews <mike@factor-50.co.uk>
2022-07-07 16:50:17 +0100

    Remove unused parameter

--- HelpMyStreetFE/HelpMyStreetFE/src/js/shared/date-picker.js
+++ HelpMyStreetFE/HelpMyStreetFE/src/js/shared/date-picker.js
@@ -9,7 +9,7 @@ export function datepickerLoad(id, errorId, dateValidationScheme = dateValidatio
 
 
     datepicker.addEventListener("focusout", function (e) {
-      validateDate(this.value, id, errorId, dateValidationScheme);
+      validateDate(this.value, errorId, dateValidationScheme);
     });
 
      // event listner to add slashes whilst entering input
@@ -31,7 +31,7 @@ export function datepickerLoad(id, errorId, dateValidationScheme = dateValidatio
 
 }
 
-export function validateDate(val, id, errorId, dateValidationScheme = dateValidationSchemes.OVER_18) {
+export function validateDate(val, errorId, dateValidationScheme = dateValidationSchemes.OVER_18) {
     var regexFormatString = RegExp(/^(([0-9])|([0-2][0-9])|([3][0-1]))\ (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\ \d{4}$/);
     var regex = RegExp(/^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/);
     let stringDate = regexFormatString.test(val);
