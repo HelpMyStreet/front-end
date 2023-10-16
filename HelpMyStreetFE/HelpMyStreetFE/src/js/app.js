40ee9232d5eb7fbf66d365d8bf651ad8f299872e
Mike Matthews <mike@factor-50.co.uk>
2023-10-16 16:54:46 +0100

    Remove unnecessary scripts

--- HelpMyStreetFE/HelpMyStreetFE/src/js/app.js
+++ HelpMyStreetFE/HelpMyStreetFE/src/js/app.js
@@ -2,14 +2,9 @@
 import 'core-js/';
 //---------------------
 import "../sass/main.scss";
-import firebase from "./firebase/index";
-import account from "./account";
 import "./shared/info-popup";
 import "./shared/site-header";
 import "./ui/filters";
-import { intialiseCookieConsent } from "./shared/cookie-helper"
-import { intialiseForgottonForm } from "./home/forgotton-password"
-import { buttonLoad, buttonUnload } from "./shared/btn";
 
 
 $(function () {
